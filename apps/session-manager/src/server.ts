ee02bfe565476151a5774438283433f16366a436
gquadrati <greta.quadrati@pagopa.it>
2024-04-18 16:52:10 +0200

    [#IOPID-1763] nix lint + update node version

--- apps/session-manager/src/server.ts
+++ apps/session-manager/src/server.ts
@@ -8,5 +8,6 @@ app.get("/", (_req: express.Request, res: express.Response) => {
 });
 
 app.listen(port, () => {
+  // eslint-disable-next-line no-console
   console.log(`Example app listening on port ${port}`);
 });
