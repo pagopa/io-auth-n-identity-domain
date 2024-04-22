04b7c46f602784c69d84c628f8c4f2bb1e405aff
Andrea Grillo <andrea.grillo@pagopa.it>
2024-04-22 17:11:31 +0200

    fix typo

--- infra/repository/data.tf
+++ infra/repository/data.tf
@@ -4,7 +4,7 @@ data "azurerm_user_assigned_identity" "identity_prod_ci" {
 }
 
 data "azurerm_user_assigned_identity" "identity_prod_cd" {
-  name                = "${local.project}-auth-n-identity-github-ci-identity"
+  name                = "${local.project}-auth-n-identity-github-cd-identity"
   resource_group_name = local.identity_resource_group_name
 }
 
