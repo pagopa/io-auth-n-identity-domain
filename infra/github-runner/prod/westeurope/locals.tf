9b7e477454ff21bab87a10727a389793403bfdcb
gquadrati <greta.quadrati@pagopa.it>
2024-04-23 12:47:17 +0200

    [#IOPID-1764] add github-runner folder

---
+++ infra/github-runner/prod/westeurope/locals.tf
@@ -0,0 +1,13 @@
+locals {
+  prefix         = "io"
+  env_short      = "p"
+
+  tags = {
+    CostCenter     = "TS310 - PAGAMENTI & SERVIZI"
+    CreatedBy      = "Terraform"
+    Environment    = "Prod"
+    Owner          = "IO"
+    ManagementTeam = "Auth&Identity"
+    Source         = "https://github.com/pagopa/io-auth-n-identity-domain/blob/main/infra/identity/prod/westeurope"
+  }
+}
