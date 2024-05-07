c8b16ce352bdd76f34dfaba99800569f4eae1af4
Andrea Grillo <andrea.grillo@pagopa.it>
2024-05-07 12:40:10 +0200

    set up identities

--- infra/identity/prod/westeurope/main.tf
+++ infra/identity/prod/westeurope/main.tf
@@ -19,15 +19,27 @@ provider "azurerm" {
   }
 }
 
-module "federated_identities" {
-  source = "github.com/pagopa/dx//infra/modules/azure_federated_identity_with_github"
+module "federated_identities_session_manager" {
+  source = "github.com/pagopa/dx//infra/modules/azure_federated_identity_with_github?ref=main"
 
-  # To-Do not working
-  # continuos_integration = {enable = false, roles = null}
+  continuos_integration = { enable = false }
 
   prefix       = local.prefix
   env_short    = local.env_short
   env          = local.session_manager_environment
+  domain       = "${local.domain}-session-manager"
+  repositories = [local.repo_name]
+  tags         = local.tags
+}
+
+module "federated_identities" {
+  source = "github.com/pagopa/dx//infra/modules/azure_federated_identity_with_github?ref=main"
+
+  continuos_delivery = { enable = false }
+
+  prefix       = local.prefix
+  env_short    = local.env_short
+  env          = local.env
   domain       = local.domain
   repositories = [local.repo_name]
   tags         = local.tags
