792366fe87c3a7b9f10a109d3b22d03e0a47eba1
Daniele Manni <dan.manni@gmail.com>
2024-05-16 15:56:53 +0200

    [#IOPID-1819] Add min age utils

--- apps/session-manager/src/utils/date.ts
+++ apps/session-manager/src/utils/date.ts
@@ -1,4 +1,4 @@
-import { format } from "date-fns";
+import { addYears, format, isAfter } from "date-fns";
 
 /**
  * Returns a formatted date.
@@ -8,3 +8,10 @@ import { format } from "date-fns";
 export function formatDate(dateStr: string): string {
   return format(dateStr, "YYYY-MM-DD");
 }
+
+/**
+ * Returns a comparator of two dates that returns true if
+ * the difference in years is at least the provided value.
+ */
+export const isOlderThan = (years: number) => (dateOfBirth: Date, when: Date) =>
+  !isAfter(addYears(dateOfBirth, years), when);
