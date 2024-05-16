335037bf236ef86ff2c067d9868d60377e4275f9
arcogabbo <arcogabbo@gmail.com>
2024-05-16 15:34:41 +0200

    migrated declaration file for passport-http-custom-bearer

---
+++ apps/session-manager/src/types/declarations/passport-http-custom-bearer.d.ts
@@ -0,0 +1,53 @@
+declare module "passport-http-custom-bearer" {
+  import passport = require("passport");
+  import express = require("express");
+  import koa = require("koa");
+
+  interface IStrategyOptions {
+    bodyName?: string;
+    headerName?: string;
+    queryName?: string;
+    scope?: string | string[] | undefined;
+    realm?: string | undefined;
+    passReqToCallback?: boolean | undefined;
+  }
+  interface IVerifyOptions {
+    message?: string | undefined;
+    scope: string | string[];
+  }
+
+  type VerifyFunction = (
+    token: string,
+    done: (error: any, user?: any, options?: IVerifyOptions | string) => void,
+  ) => void;
+
+  interface IKoaContextContainer {
+    ctx: koa.Context;
+  }
+  type KoaPassportExpressRequestMock = Partial<express.Request> &
+    IKoaContextContainer;
+
+  type VerifyFunctionWithRequest = (
+    req: express.Request,
+    token: string,
+    done: (error: any, user?: any, options?: IVerifyOptions | string) => void,
+  ) => void;
+  type VerifyFunctionWithContext = (
+    req: KoaPassportExpressRequestMock,
+    token: string,
+    done: (error: any, user?: any, options?: IVerifyOptions | string) => void,
+  ) => void;
+
+  type VerifyFunctions =
+    | VerifyFunction
+    | VerifyFunctionWithRequest
+    | VerifyFunctionWithContext;
+
+  class Strategy<T extends VerifyFunctions> implements passport.Strategy {
+    constructor(verify: VerifyFunction);
+    constructor(options: IStrategyOptions, verify: T);
+
+    name: string;
+    authenticate(req: express.Request, options?: Object): void;
+  }
+}
