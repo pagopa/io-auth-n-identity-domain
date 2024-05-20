100b2c131f997f572e04a3fd104de2cd550b2853
Daniele Manni <dan.manni@gmail.com>
2024-05-20 12:46:50 +0200

    [#IOPID-1819] Add Login service with onUserLogin handler

---
+++ apps/session-manager/src/services/login.ts
@@ -0,0 +1,36 @@
+import { pipe } from "fp-ts/lib/function";
+import * as TE from "fp-ts/TaskEither";
+import * as E from "fp-ts/Either";
+import { errorsToReadableMessages } from "@pagopa/ts-commons/lib/reporters";
+import { UserLoginParams } from "@pagopa/io-functions-app-sdk/UserLoginParams";
+import * as RTE from "fp-ts/ReaderTaskEither";
+import { FnAppAPIRepositoryDeps } from "../repositories/fn-app-api";
+
+export const onUserLogin: (
+  data: UserLoginParams,
+) => RTE.ReaderTaskEither<FnAppAPIRepositoryDeps, Error, boolean> =
+  (body) => (deps) =>
+    pipe(
+      TE.tryCatch(
+        () => deps.fnAppAPIClient.startNotifyLoginProcess({ body }),
+        (_) =>
+          new Error(`Error calling startNotifyLoginProcess: ${E.toError(_)}`),
+      ),
+      TE.chainEitherKW(
+        E.mapLeft(
+          (_) =>
+            new Error(
+              `Error decoding startNotifyLoginProcess response: ${errorsToReadableMessages(
+                _,
+              )}`,
+            ),
+        ),
+      ),
+      TE.chain((response) =>
+        response.status === 202
+          ? TE.of(true)
+          : TE.left(
+              new Error(`startNotifyLoginProcess returned ${response.status}`),
+            ),
+      ),
+    );
