7f5408948244d17a021d30a09a3bffc132d2c0ef
Andrea Grillo <andrea.grillo@pagopa.it>
2024-05-20 17:15:52 +0200

    [EC-296] Update CI/CD infra paths and templates (#38)

--- infra/resources/prod/main.tf
+++ infra/resources/prod/main.tf
@@ -3,16 +3,15 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">= 3.96.0"
+      version = "<= 3.104.0"
     }
-
   }
 
   backend "azurerm" {
     resource_group_name  = "terraform-state-rg"
     storage_account_name = "tfinfprodio"
     container_name       = "terraform-state"
-    key                  = "io-auth-n-identity-domain.resources.prod.westeurope.tfstate"
+    key                  = "io-auth-n-identity-domain.resources.prod.tfstate"
   }
 }
 
