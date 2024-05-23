21e33f039581b744cb778bfcd6b3127edbbec9db
Daniele Manni <dan.manni@gmail.com>
2024-05-23 11:30:17 +0200

    [#IOPID-1866] Improve tests with more readable pipeline

--- apps/session-manager/src/repositories/__tests__/lollipop-revoke-queue.spec.ts
+++ apps/session-manager/src/repositories/__tests__/lollipop-revoke-queue.spec.ts
@@ -1,8 +1,8 @@
 import { describe, test, expect, beforeEach, vi } from "vitest";
-import * as TE from "fp-ts/TaskEither";
 import { pipe } from "fp-ts/lib/function";
 import * as E from "fp-ts/Either";
 import { RevokeAssertionRefInfo } from "@pagopa/io-functions-commons/dist/src/entities/revoke_assertion_ref_info";
+import { QueueSendMessageResponse } from "@azure/storage-queue";
 import {
   mockQueueClient,
   mockSendMessage,
@@ -19,33 +19,32 @@ describe("LollipopRevokeRepo#revokePreviousAssertionRef", () => {
   const expectedMessage = base64EncodeObject(
     RevokeAssertionRefInfo.encode({ assertion_ref: anAssertionRef }),
   );
+  const mockedDependencies = {
+    lollipopRevokeQueueClient: mockQueueClient,
+  };
   test("should enqueue the message and return the expected response", async () => {
     const expectedResponse = {
       messageId: "aMessageId",
       requestId: "aRequestId",
-    };
+    } as unknown as QueueSendMessageResponse;
     mockSendMessage.mockResolvedValueOnce(expectedResponse);
     const result = await pipe(
-      revokePreviousAssertionRef(anAssertionRef)({
-        lollipopRevokeQueueClient: mockQueueClient,
-      }),
-      TE.map((value) => expect(value).toEqual(expectedResponse)),
+      mockedDependencies,
+      revokePreviousAssertionRef(anAssertionRef),
     )();
     expect(mockSendMessage).toBeCalledWith(expectedMessage);
-    expect(E.isRight(result)).toBeTruthy();
+    expect(result).toEqual(E.right(expectedResponse));
   });
 
   test("should returns an error if the queue client throws", async () => {
     const expectedError = new Error("Network Error");
     mockSendMessage.mockRejectedValue(expectedError);
     const result = await pipe(
-      revokePreviousAssertionRef(anAssertionRef)({
-        lollipopRevokeQueueClient: mockQueueClient,
-      }),
-      TE.mapLeft((value) => expect(value).toEqual(expectedError)),
+      mockedDependencies,
+      revokePreviousAssertionRef(anAssertionRef),
     )();
 
     expect(mockSendMessage).toBeCalledWith(expectedMessage);
-    expect(E.isLeft(result)).toBeTruthy();
+    expect(result).toEqual(E.left(expectedError));
   });
 });
