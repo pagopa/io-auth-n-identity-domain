863693bd5f14034ce31605e7432884b53a1b8a8b
gquadrati <greta.quadrati@pagopa.it>
2024-05-27 12:09:12 +0200

    [#IOPID-1876] add AppInsightsConfig

---
+++ apps/session-manager/src/config/appinsights.ts
@@ -0,0 +1,23 @@
+import * as O from "fp-ts/Option";
+import * as E from "fp-ts/Either";
+import { pipe } from "fp-ts/lib/function";
+import { NonEmptyString } from "@pagopa/ts-commons/lib/strings";
+
+export const APPLICATIONINSIGHTS_CONNECTION_STRING = O.fromNullable(
+  process.env.APPLICATIONINSIGHTS_CONNECTION_STRING,
+);
+
+export const APPINSIGHTS_CLOUD_ROLE_NAME = pipe(
+  process.env.APPINSIGHTS_CLOUD_ROLE_NAME,
+  NonEmptyString.decode,
+  E.getOrElseW(() => undefined),
+);
+
+export const APPINSIGHTS_DISABLED = process.env.APPINSIGHTS_DISABLED === "true";
+
+// Application insights sampling percentage
+const DEFAULT_APPINSIGHTS_SAMPLING_PERCENTAGE = 5;
+export const APPINSIGHTS_SAMPLING_PERCENTAGE = process.env
+  .APPINSIGHTS_SAMPLING_PERCENTAGE
+  ? parseInt(process.env.APPINSIGHTS_SAMPLING_PERCENTAGE, 10)
+  : DEFAULT_APPINSIGHTS_SAMPLING_PERCENTAGE;
