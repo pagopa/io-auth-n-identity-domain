bd24121ccdad7668169e784f850a5c4b62b7b904
arcogabbo <arcogabbo@gmail.com>
2024-06-06 11:13:41 +0200

    [#IOPID-1767] added opex fed identities

--- infra/identity/prod/westeurope/main.tf
+++ infra/identity/prod/westeurope/main.tf
@@ -44,3 +44,48 @@ module "federated_identities" {
   repositories = [local.repo_name]
   tags         = local.tags
 }
+
+module "opex_federated_identities" {
+  source = "github.com/pagopa/dx//infra/modules/azure_federated_identity_with_github?ref=main"
+
+  prefix       = local.prefix
+  env_short    = local.env_short
+  env          = "opex-${local.env}"
+  domain       = "${local.domain}-opex"
+  repositories = [local.repo_name]
+
+  continuos_integration = {
+    enable = true
+    roles = {
+      subscription = ["Reader"]
+      resource_groups = {
+        dashboards = [
+          "Reader"
+        ],
+        terraform-state-rg = [
+          "Storage Blob Data Reader",
+          "Reader and Data Access"
+        ]
+      }
+    }
+  }
+
+  continuos_delivery = {
+    enable = true
+
+    roles = {
+      subscription = ["Reader"]
+      resource_groups = {
+        dashboards = [
+          "Contributor"
+        ],
+        terraform-state-rg = [
+          "Storage Blob Data Contributor",
+          "Reader and Data Access"
+        ]
+      }
+    }
+  }
+
+  tags = local.tags
+}
