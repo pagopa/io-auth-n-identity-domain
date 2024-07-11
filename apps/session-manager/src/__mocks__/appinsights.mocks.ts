a16aa3c1c824367602ab6d3fd75825e84de57d7f
gquadrati <greta.quadrati@pagopa.it>
2024-07-11 14:39:19 +0200

    [#IOPID-1769] add tests

--- apps/session-manager/src/__mocks__/appinsights.mocks.ts
+++ apps/session-manager/src/__mocks__/appinsights.mocks.ts
@@ -1,8 +1,12 @@
+import * as appInsights from "applicationinsights";
+
 import { vi } from "vitest";
 
 // TODO: Add types when appinsights lib will be added
 export const mockTrackEvent = vi.fn();
+export const mockTrackDependency = vi.fn();
 
 export const mockedAppinsightsTelemetryClient = {
   trackEvent: mockTrackEvent,
-};
+  trackDependency: mockTrackDependency,
+} as any as appInsights.TelemetryClient;
---
+++ apps/session-manager/src/utils/__tests__/redis-trace-wrapper.spec.ts
@@ -0,0 +1,90 @@
+import { vi, beforeEach, describe, expect, test, beforeAll } from "vitest";
+
+import { createWrappedRedisClusterClient } from "../redis-trace-wrapper";
+
+import {
+  mockedAppinsightsTelemetryClient,
+  mockTrackDependency,
+} from "../../__mocks__/appinsights.mocks";
+
+const mockedAppInsightsClient = mockedAppinsightsTelemetryClient;
+
+const mockGet = vi.fn(async () => "42");
+
+vi.mock("redis", () => ({
+  createCluster: () => ({
+    get: mockGet,
+  }),
+}));
+
+beforeEach(() => {
+  vi.clearAllMocks();
+});
+
+describe("createWrappedRedisClusterClient |> trace enabled", () => {
+  const wrappedClient = createWrappedRedisClusterClient(
+    {} as any,
+    true,
+    "TEST",
+    mockedAppInsightsClient,
+  );
+
+  test("should call `trackDependency` when the request completed successfully", async () => {
+    const result = await wrappedClient.get("TEST");
+
+    expect(result).toEqual("42");
+    expect(mockTrackDependency).toHaveBeenCalledWith({
+      target: "Redis Cluster - TEST",
+      name: "get",
+      data: "",
+      resultCode: "",
+      duration: expect.any(Number),
+      success: true,
+      dependencyTypeName: "REDIS",
+    });
+  });
+
+  test("should call `trackDependency` when the request failed", async () => {
+    mockGet.mockRejectedValueOnce({});
+
+    try {
+      await wrappedClient.get("TEST");
+    } catch (e) {}
+
+    expect(mockTrackDependency).toHaveBeenCalledWith({
+      target: "Redis Cluster - TEST",
+      name: "get",
+      data: "",
+      resultCode: "ERROR",
+      duration: expect.any(Number),
+      success: false,
+      dependencyTypeName: "REDIS",
+    });
+  });
+});
+
+describe("createWrappedRedisClusterClient |> trace disabled", () => {
+  const wrappedClient = createWrappedRedisClusterClient(
+    {} as any,
+    false,
+    "TEST",
+    mockedAppInsightsClient,
+  );
+
+  test("should NOT call `trackDependency` when the request completed successfully", async () => {
+    const result = await wrappedClient.get("TEST");
+
+    expect(result).toEqual("42");
+    expect(mockTrackDependency).not.toHaveBeenCalled();
+  });
+
+  test("should NOT call `trackDependency` when the request failed", async () => {
+    mockGet.mockRejectedValueOnce({});
+
+    try {
+      await wrappedClient.get("TEST");
+    } catch (e) {}
+
+    expect(mockTrackDependency).not.toHaveBeenCalled();
+  });
+});
