9b835420bf58fa06aff11636dcac5bb487f3a73f
Andrea Grillo <andrea.grillo@pagopa.it>
2024-10-24 11:03:09 +0200

    [CES-206] Move identities to ItalyNorth (#111)

--- infra/repository/github_environment_session_manager_cd.tf
+++ infra/repository/github_environment_session_manager_cd.tf
@@ -1,4 +1,4 @@
-resource "github_repository_environment" "github_repository_environment_session_manager_prod_cd" {
+resource "github_repository_environment" "session_manager_prod_cd" {
   environment = "session-manager-prod-cd"
   repository  = github_repository.this.name
 
@@ -6,22 +6,30 @@ resource "github_repository_environment" "github_repository_environment_session_
     protected_branches     = false
     custom_branch_policies = true
   }
+
+  reviewers {
+    teams = matchkeys(
+      data.github_organization_teams.all.teams[*].id,
+      data.github_organization_teams.all.teams[*].slug,
+      local.cd.reviewers_teams
+    )
+  }
 }
 
-resource "github_actions_environment_secret" "env_prod_cd_secrets" {
+resource "github_actions_environment_secret" "session_manager_prod_cd" {
   for_each = local.session_manager_cd.secrets
 
   repository      = github_repository.this.name
-  environment     = github_repository_environment.github_repository_environment_session_manager_prod_cd.environment
+  environment     = github_repository_environment.session_manager_prod_cd.environment
   secret_name     = each.key
   plaintext_value = each.value
 }
 
-resource "github_actions_environment_variable" "env_prod_cd_variables" {
+resource "github_actions_environment_variable" "session_manager_prod_cd" {
   for_each = local.session_manager_cd.variables
 
   repository    = github_repository.this.name
-  environment   = github_repository_environment.github_repository_environment_session_manager_prod_cd.environment
+  environment   = github_repository_environment.session_manager_prod_cd.environment
   variable_name = each.key
   value         = each.value
 }
