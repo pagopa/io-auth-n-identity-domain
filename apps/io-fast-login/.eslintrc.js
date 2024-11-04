f1d33ef3b580c8c57d1bddfc5e5caabf0a971de9
Salvatore Laiso <salvatore.laiso@it.ey.com>
2024-11-04 11:50:17 +0100

    chore: fix linting

--- apps/io-fast-login/.eslintrc.js
+++ apps/io-fast-login/.eslintrc.js
@@ -1,26 +1,28 @@
 module.exports = {
     "env": {
-        "browser": true,
-        "es6": true,
+        "es2021": true,
         "node": true
     },
     "ignorePatterns": [
         "node_modules",
-        "generated",
+        "**/generated",
         "**/__tests__/*",
         "**/__mocks__/*",
         "*.d.ts",
         "docker",
         "jest.config.js",
         "**/__integrations__/*",
+        "*.yaml",
+        "dist",
     ],
     "parser": "@typescript-eslint/parser",
     "parserOptions": {
-        "project": "tsconfig.json",
-        "sourceType": "module"
+        "project": "./tsconfig.eslint.json",
+        "sourceType": "module",
+        "tsconfigRootDir": __dirname,
     },
     "extends": [
-        "@pagopa/eslint-config/strong",
+        "eslint-config-monorepo/index.js",
     ],
     "rules": {
         "@typescript-eslint/consistent-type-definitions": ["error", "type"]
