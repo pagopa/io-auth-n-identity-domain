461f55d357ae35f68e5a598d1df75712294936aa
Daniele Manni <dan.manni@gmail.com>
2024-11-20 12:22:44 +0100

    [#IOPID-1969] Add appgateway and shared app service data

--- infra/resources/prod/data.tf
+++ infra/resources/prod/data.tf
@@ -20,3 +20,19 @@ data "azurerm_subnet" "private_endpoints_subnet" {
   virtual_network_name = data.azurerm_virtual_network.itn_common.name
   resource_group_name  = data.azurerm_virtual_network.itn_common.resource_group_name
 }
+
+##########################
+# APP GATEWAY DATA SOURCE
+##########################
+data "azurerm_application_gateway" "app_gateway" {
+  name                = format("%s-appgateway", local.common_project)
+  resource_group_name = local.appgw_resource_group_name
+}
+
+##########################
+# SHARED APP SERVICE PLAN
+##########################
+data "azurerm_app_service_plan" "shared_plan_itn" {
+  name                = format("%s-%s-shared-asp-01", local.project, "citizen-auth")
+  resource_group_name = format("%s-%s-shared-rg-01", local.project, "citizen-auth")
+}
