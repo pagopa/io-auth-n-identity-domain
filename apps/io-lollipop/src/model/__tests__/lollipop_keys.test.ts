7598064ce61e06b839bca2738bb44c68d7fbf1d9
Salvatore Laiso <32564922+salvatorelaiso@users.noreply.github.com>
2024-12-09 16:07:27 +0100

    [#IOPID-2458] `io-functions-lollipop` migration into `io-auth-n-identity-domain` (#130)
    Co-authored-by: AleDore <alessio85.dore@gmail.com>
    Co-authored-by: pagopa-github-bot <github-bot@pagopa.it>
    Co-authored-by: gquadrati <greta.quadrati@pagopa.it>
    Co-authored-by: Alessio Dore <57567806+AleDore@users.noreply.github.com>
    Co-authored-by: Gabriele Mendolia <arcogabbo@gmail.com>
    Co-authored-by: Greta Quadrati <75862507+gquadrati@users.noreply.github.com>
    Co-authored-by: garma00 <giovanniarmaleo00@gmail.com>
    Co-authored-by: Daniele Manni <BurnedMarshal@users.noreply.github.com>
    Co-authored-by: Andrea Cuiuli <andrea.cuiuli@pagopa.it>
    Co-authored-by: fabriziopapi <fabrizio.papi@pagopa.it>
    Co-authored-by: Daniele Manni <dan.manni@gmail.com>
    Co-authored-by: anubi <80677434+fabriziopapi@users.noreply.github.com>
    Co-authored-by: Michael Disaro <michael.disaro@gmail.com>
    Co-authored-by: renovate-pagopa[bot] <164534245+renovate-pagopa[bot]@users.noreply.github.com>
    Co-authored-by: Salvatore Laiso <salvatore.laiso@it.ey.com>

---
+++ apps/io-lollipop/src/model/__tests__/lollipop_keys.test.ts
@@ -0,0 +1,111 @@
+import { afterAll, beforeAll, beforeEach, describe, expect, it, vi } from "vitest";
+import * as E from "fp-ts/lib/Either";
+import * as tk from "timekeeper";
+
+import {
+  LolliPOPKeysModel,
+  TTL_VALUE_AFTER_UPDATE,
+  TTL_VALUE_FOR_RESERVATION
+} from "../lollipop_keys";
+import { PubKeyStatusEnum } from "../../generated/definitions/internal/PubKeyStatus";
+import {
+  aLolliPopPubKeys,
+  aPendingLolliPopPubKeys,
+  aRetrievedLolliPopPubKeys,
+  mockContainer
+} from "../../__mocks__/lollipopkeysMock";
+
+beforeEach(() => {
+  vi.clearAllMocks();
+});
+
+const mockedNowTime = new Date(TTL_VALUE_FOR_RESERVATION * 1000);
+
+beforeAll(() => {
+  tk.freeze(mockedNowTime);
+});
+
+afterAll(() => {
+  tk.reset();
+});
+
+describe("create", () => {
+  it("GIVEN a working PopModel instance and a pendingLolliPopPubKeys, WHEN create is called, THEN upsert should be called with ttl equals to 15 minutes", async () => {
+    const mockedContainer = mockContainer();
+    const model = new LolliPOPKeysModel(mockedContainer.container);
+    await model.create(aPendingLolliPopPubKeys)();
+    expect(mockedContainer.mock.create).toHaveBeenCalledWith(
+      expect.objectContaining({ ttl: TTL_VALUE_FOR_RESERVATION }),
+      expect.anything()
+    );
+  });
+
+  it("GIVEN a working PopModel instance should fail with a COSMOS_ERROR_RESPONSE if a document with same modelId exists", async () => {
+    const mockedContainer = mockContainer();
+    mockedContainer.mock.fetchAll.mockImplementationOnce(async () => ({
+      resources: [aRetrievedLolliPopPubKeys]
+    }));
+    const model = new LolliPOPKeysModel(mockedContainer.container);
+    const result = await model.create(aPendingLolliPopPubKeys)();
+
+    expect(mockedContainer.mock.create).not.toHaveBeenCalled();
+    expect(mockedContainer.mock.fetchAll).toHaveBeenCalled();
+    expect(E.isLeft(result)).toBeTruthy();
+    if (E.isLeft(result)) {
+      expect(result.left.kind).toEqual("COSMOS_CONFLICT_RESPONSE");
+    }
+  });
+});
+
+describe("upsert", () => {
+  it("Should return a COSMOS_DECODING_ERROR if the generated ttl was negative", async () => {
+    const mockedContainer = mockContainer();
+    mockedContainer.mock.fetchAll.mockImplementationOnce(async () => ({
+      resources: [
+        { ...aRetrievedLolliPopPubKeys, ttl: TTL_VALUE_FOR_RESERVATION - 2 }
+      ]
+    }));
+    const model = new LolliPOPKeysModel(mockedContainer.container);
+    const result = await model.upsert(aLolliPopPubKeys)();
+    expect(E.isLeft(result)).toBeTruthy();
+    if (E.isLeft(result)) {
+      expect(result.left.kind).toEqual("COSMOS_DECODING_ERROR");
+    }
+    expect(mockedContainer.mock.create).not.toHaveBeenCalled();
+  });
+
+  it("GIVEN a working PopModel instance and a pendingLolliPopPubKeys, WHEN upsert is called, THEN super.upsert should be called with ttl equals to 2y", async () => {
+    const mockedContainer = mockContainer();
+    mockedContainer.mock.fetchAll.mockImplementationOnce(async () => ({
+      resources: [
+        { ...aRetrievedLolliPopPubKeys, status: PubKeyStatusEnum.PENDING }
+      ]
+    }));
+    const model = new LolliPOPKeysModel(mockedContainer.container);
+    await model.upsert(aLolliPopPubKeys)();
+    expect(mockedContainer.mock.create).toHaveBeenCalledWith(
+      expect.objectContaining({ ttl: TTL_VALUE_AFTER_UPDATE }),
+      expect.anything()
+    );
+  });
+
+  it("GIVEN a previous version with a ttl, the new version should have a ttl with the remaining time to reach the previous one", async () => {
+    const aMockedTtl = TTL_VALUE_FOR_RESERVATION;
+    const mockedContainer = mockContainer();
+    mockedContainer.mock.fetchAll.mockImplementationOnce(async () => ({
+      resources: [{ ...aRetrievedLolliPopPubKeys, ttl: aMockedTtl }]
+    }));
+    const model = new LolliPOPKeysModel(mockedContainer.container);
+    await model.upsert(aLolliPopPubKeys)();
+    expect(mockedContainer.mock.create).toHaveBeenCalledWith(
+      expect.objectContaining({
+        ttl:
+          // eslint
+          aRetrievedLolliPopPubKeys._ts +
+          aMockedTtl -
+          mockedNowTime.getTime() / 1000
+      }),
+      expect.anything()
+    );
+  });
+});
