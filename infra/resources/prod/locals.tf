013cb11b498444738b854ad3f2c73cf16b41273f
Daniele Manni <BurnedMarshal@users.noreply.github.com>
2024-12-09 13:57:50 +0100

    [#IOPID-2540] Import `ioweb-profile-backend` APIM api into monorepo (#129)

--- infra/resources/prod/locals.tf
+++ infra/resources/prod/locals.tf
@@ -16,6 +16,13 @@ locals {
   appgw_resource_group_name           = "${local.common_project}-rg-external"
   immutable_audit_logs_container_name = "auditlogs"
 
+  apim_v2_name                 = "${local.prefix}-${local.env_short}-apim-v2-api"
+  apim_resource_group_name     = "${local.prefix}-${local.env_short}-rg-internal"
+  apim_itn_name                = "${local.prefix}-${local.env_short}-itn-apim-01"
+  apim_itn_resource_group_name = "${local.prefix}-${local.env_short}-itn-common-rg-01"
+
+  io_web_profile_bff_basepath = "ioweb/backend/api/v1"
+
   tags = {
     CreatedBy      = "Terraform"
     Environment    = "Prod"
