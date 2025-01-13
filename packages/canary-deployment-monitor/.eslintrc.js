a9730b0f80411fea9fb23631e4461555f01008b7
Daniele Manni <BurnedMarshal@users.noreply.github.com>
2025-01-13 12:20:13 +0100

    Automated Canary deployment (#159)

---
+++ packages/canary-deployment-monitor/.eslintrc.js
@@ -0,0 +1,20 @@
+/** @type {import("eslint").Linter.Config} */
+module.exports = {
+    env: {
+      es2021: true,
+      node: true
+    },
+    extends: ["eslint-config-monorepo/index.js"],
+    parserOptions: {
+      project: "./tsconfig.eslint.json",
+      tsconfigRootDir: __dirname,
+    },
+    ignorePatterns: [
+      "*.yaml",
+      "src/generated/**/*",
+      "dist/**/*",
+    ],
+    rules: {
+      "max-classes-per-file": "off",
+    }
+  };
