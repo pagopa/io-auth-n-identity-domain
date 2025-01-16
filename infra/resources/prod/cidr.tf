fa4cb33888545238090b5043ccb0f0e121735cd0
gquadrati <greta.quadrati@pagopa.it>
2025-01-16 09:35:05 +0100

    add function_public to iac

--- infra/resources/prod/cidr.tf
+++ infra/resources/prod/cidr.tf
@@ -1,5 +1,10 @@
 locals {
+
+  # You can retrieve the list of current defined subnets using the CLI command
+  # az network vnet subnet list --subscription PROD-IO --vnet-name io-p-itn-common-vnet-01 --resource-group io-p-itn-common-rg-01 --output table
+  # and thus define new CIDRs according to the unallocated address space
   cidr_subnet_fn_lv          = "10.20.20.0/26"
   cidr_subnet_fn_lollipop    = "10.20.20.64/26"
   cidr_subnet_fn_web_profile = "10.20.24.0/26"
+  cidr_subnet_fn_public      = "10.20.24.64/26"
 }
--- infra/resources/prod/cosmos.tf
+++ infra/resources/prod/cosmos.tf
@@ -6,3 +6,8 @@ data "azurerm_cosmosdb_account" "cosmos_citizen_auth" {
   name                = "${local.common_project}-citizen-auth-account"
   resource_group_name = data.azurerm_resource_group.core_domain_data_rg.name
 }
+
+data "azurerm_cosmosdb_account" "cosmos_api" {
+  name                = format("%s-cosmos-api", local.common_project)
+  resource_group_name = format("%s-rg-internal", local.common_project)
+}
