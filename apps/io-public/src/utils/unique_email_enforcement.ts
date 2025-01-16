8703c202738b05849053e192c4e3c24d08561880
Salvatore Laiso <salvatore.laiso@it.ey.com>
2025-01-16 15:16:36 +0100

    chore: move `utils` folder under `src`

--- apps/io-public/src/utils/unique_email_enforcement.ts
+++ apps/io-public/src/utils/unique_email_enforcement.ts
@@ -1,6 +1,6 @@
 import { TableClient } from "@azure/data-tables";
 
-import { getConfigOrThrow } from "../utils/config";
+import { getConfigOrThrow } from "./config";
 
 const config = getConfigOrThrow();
 
