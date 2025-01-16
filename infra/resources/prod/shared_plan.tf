980bc989cb0ebb71e86acd0a33b91cab2a572dda
gquadrati <greta.quadrati@pagopa.it>
2025-01-16 12:14:37 +0100

    add fn-public rules to autoscale settings

--- infra/resources/prod/shared_plan.tf
+++ infra/resources/prod/shared_plan.tf
@@ -94,6 +94,28 @@ resource "azurerm_monitor_autoscale_setting" "shared_plan_autoscale" {
 
     rule {
       metric_trigger {
+        metric_name              = "Requests"
+        metric_resource_id       = module.function_public.function_app.function_app.id
+        metric_namespace         = "microsoft.web/sites"
+        time_grain               = "PT1M"
+        statistic                = "Average"
+        time_window              = "PT5M"
+        time_aggregation         = "Average"
+        operator                 = "GreaterThan"
+        threshold                = 3000
+        divide_by_instance_count = false
+      }
+
+      scale_action {
+        direction = "Increase"
+        type      = "ChangeCount"
+        value     = "2"
+        cooldown  = "PT5M"
+      }
+    }
+
+    rule {
+      metric_trigger {
         metric_name              = "CpuPercentage"
         metric_resource_id       = azurerm_app_service_plan.shared_plan.id
         metric_namespace         = "microsoft.web/serverfarms"
@@ -140,6 +162,28 @@ resource "azurerm_monitor_autoscale_setting" "shared_plan_autoscale" {
       metric_trigger {
         metric_name              = "Requests"
         metric_resource_id       = module.function_web_profile.function_app.function_app.id
+        metric_namespace         = "microsoft.web/sites"
+        time_grain               = "PT1M"
+        statistic                = "Average"
+        time_window              = "PT5M"
+        time_aggregation         = "Average"
+        operator                 = "LessThan"
+        threshold                = 2000
+        divide_by_instance_count = false
+      }
+
+      scale_action {
+        direction = "Decrease"
+        type      = "ChangeCount"
+        value     = "1"
+        cooldown  = "PT20M"
+      }
+    }
+
+    rule {
+      metric_trigger {
+        metric_name              = "Requests"
+        metric_resource_id       = module.function_public.function_app.function_app.id
         metric_namespace         = "microsoft.web/sites"
         time_grain               = "PT1M"
         statistic                = "Average"
