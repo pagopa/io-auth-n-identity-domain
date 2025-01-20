d63bc6f86dd3dc0019d3475f894edad911b78e57
Greta Quadrati <75862507+gquadrati@users.noreply.github.com>
2025-01-20 10:34:57 +0100

    [IOPID-2464] Migrate fn-public IaC (#161)
    Co-authored-by: Gabriele Mendolia <arcogabbo@gmail.com>

--- infra/resources/prod/storage.tf
+++ infra/resources/prod/storage.tf
@@ -7,3 +7,13 @@ data "azurerm_storage_account" "immutable_spid_logs_storage" {
   name                = replace(format("%s-ioweb-spid-logs-im-st", local.weu_project), "-", "")
   resource_group_name = data.azurerm_resource_group.ioweb_storage_rg.name
 }
+
+data "azurerm_storage_account" "citizen_auth_common" {
+  name                = replace(format("%s-%s-st", local.weu_project, local.legacy_domain), "-", "")
+  resource_group_name = data.azurerm_resource_group.core_domain_data_rg.name
+}
+
+data "azurerm_storage_account" "storage_api" {
+  name                = replace(format("%s-st-api", local.common_project), "-", "")
+  resource_group_name = format("%s-rg-internal", local.common_project)
+}
