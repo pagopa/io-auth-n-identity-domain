b6b8fce8b921166642f193b936ce9ee96f65da8e
Gianmaria Scorza <gianmaria@scorza.dev>
2025-01-20 12:01:13 +0100

    using an already existent secret to test plan

--- infra/modules/io_profile_api/data.tf
+++ infra/modules/io_profile_api/data.tf
@@ -6,7 +6,9 @@ data "azurerm_api_management" "apim" {
 
 # Secrets
 
+# TODO: Add Secret into kv ad use it here
+# TODO: this is provisional
 data "azurerm_key_vault_secret" "io_fn_profile_key_secret_v2" {
-  name         = "fn-profile-KEY-APIM"
+  name         = "fn3public-KEY-APIM" # TODO: "fn-profile-KEY-APIM"
   key_vault_id = var.key_vault_common_id
 }
