743f527e9b38859fce61dec0962991a021c8bbd5
Gianmaria Scorza <gianmaria@scorza.dev>
2025-01-20 15:09:50 +0100

    add right profile-fn secrets

--- infra/modules/io_profile_api/data.tf
+++ infra/modules/io_profile_api/data.tf
@@ -6,9 +6,7 @@ data "azurerm_api_management" "apim" {
 
 # Secrets
 
-# TODO: Add Secret into kv ad use it here
-# TODO: this is provisional
 data "azurerm_key_vault_secret" "io_fn_profile_key_secret_v2" {
-  name         = "fn3public-KEY-APIM" # TODO: "fn-profile-KEY-APIM"
+  name         = "fn-profile-KEY-APIM"
   key_vault_id = var.key_vault_common_id
 }
