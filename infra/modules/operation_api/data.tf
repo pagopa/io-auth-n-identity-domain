9b4fa5bbe31a97a865461e78c33c9a0d2480ed37
Gianmaria Scorza <gianmaria@scorza.dev>
2025-01-22 10:33:22 +0100

    rename pt3

--- infra/modules/operation_api/data.tf
+++ infra/modules/operation_api/data.tf
@@ -6,7 +6,7 @@ data "azurerm_api_management" "apim" {
 
 # Secrets
 
-data "azurerm_key_vault_secret" "io_fn_profile_key_secret_v2" {
+data "azurerm_key_vault_secret" "io_fn_profile_key_secret" {
   name         = "fn-profile-KEY-APIM"
   key_vault_id = var.key_vault_common_id
 }
