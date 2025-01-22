ee34752f74529241028d4f818524aa904ebbc1b5
Gianmaria Scorza <gianmaria@scorza.dev>
2025-01-22 10:30:44 +0100

    expose also in the new ITN APIM

--- infra/resources/prod/api_operation_weu.tf
+++ infra/resources/prod/api_operation_weu.tf
@@ -1,7 +1,7 @@
 module "io_profile_apim_api_weu" {
   source = "../../modules/operation_api"
 
-  apim_name                = local.apim_itn_name
+  apim_name                = local.apim_v2_name
   apim_resource_group_name = local.apim_resource_group_name
 
   key_vault_common_id = data.azurerm_key_vault.common_kv.id
