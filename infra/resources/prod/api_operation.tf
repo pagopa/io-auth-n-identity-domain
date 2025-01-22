daf87e38fb1dce7c7ff7683446bcdb2f27bd2717
Gianmaria Scorza <gianmaria@scorza.dev>
2025-01-22 10:40:22 +0100

    set right rg name on it operation_api

--- infra/resources/prod/api_operation.tf
+++ infra/resources/prod/api_operation.tf
@@ -2,7 +2,7 @@ module "io_profile_apim_api_itn" {
   source = "../../modules/operation_api"
 
   apim_name                = local.apim_itn_name
-  apim_resource_group_name = local.apim_resource_group_name
+  apim_resource_group_name = local.apim_itn_resource_group_name
 
   key_vault_common_id = data.azurerm_key_vault.common_kv.id
 
