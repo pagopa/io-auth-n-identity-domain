d62cf61c592eba99e35ebee79daef61f71436b28
Krusty93 <andrea.grillo@pagopa.it>
2025-01-27 11:57:43 +0100

    tuning roles

--- infra/identity/prod/main.tf
+++ infra/identity/prod/main.tf
@@ -30,6 +30,29 @@ module "federated_identities" {
   repositories = [local.repo_name]
   tags         = local.tags
 
+  continuos_integration = {
+    enable = true
+    roles = {
+      subscription = [
+        "Reader",
+        "Reader and Data Access",
+        "PagoPA IaC Reader",
+        "DocumentDB Account Contributor"
+      ]
+      resource_groups = {
+        terraform-state-rg = [
+          "Storage Blob Data Contributor"
+        ]
+        io-p-rg-internal = [
+          "API Management Service Contributor"
+        ]
+        io-p-itn-common-rg-01 = [
+          "API Management Service Contributor"
+        ]
+      }
+    }
+  }
+
   continuos_delivery = {
     enable = true
     roles = {
