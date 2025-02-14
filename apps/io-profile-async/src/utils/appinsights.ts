d946d768a693dd2e9f5066151a154ce59ff0ad19
Salvatore Laiso <32564922+salvatorelaiso@users.noreply.github.com>
2025-02-14 11:44:01 +0100

    Update apps/io-profile-async/src/utils/appinsights.ts
    Co-authored-by: Gabriele Mendolia <arcogabbo@gmail.com>

--- apps/io-profile-async/src/utils/appinsights.ts
+++ apps/io-profile-async/src/utils/appinsights.ts
@@ -12,7 +12,7 @@ import { pipe } from "fp-ts/lib/function";
 
 // the internal function runtime has MaxTelemetryItem per second set to 20 by default
 // @see https://github.com/Azure/azure-functions-host/blob/master/src/WebJobs.Script/Config/ApplicationInsightsLoggerOptionsSetup.cs#L29
-const DEFAULT_SAMPLING_PERCENTAGE = 20;
+const DEFAULT_SAMPLING_PERCENTAGE = 5;
 
 // Avoid to initialize Application Insights more than once
 export const initTelemetryClient = (
