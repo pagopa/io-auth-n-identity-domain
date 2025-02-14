8cda80b59e95b39c4103959b00dde6f4d8216403
Salvatore Laiso <salvatore.laiso@it.ey.com>
2025-02-14 09:09:17 +0100

    fix: improve tests expected

--- apps/io-profile-async/src/functions/__tests__/info.test.ts
+++ apps/io-profile-async/src/functions/__tests__/info.test.ts
@@ -1,6 +1,7 @@
 import { describe, expect, it, vi } from "vitest";
 import { Database } from "@azure/cosmos";
 import * as E from "fp-ts/lib/Either";
+import * as H from "@pagopa/handler-kit";
 import * as TE from "fp-ts/lib/TaskEither";
 import { makeInfoHandler } from "../info";
 import { httpHandlerInputMocks } from "../__mocks__/handlerMocks";
@@ -36,10 +37,12 @@ describe("Info handler", () => {
     })();
 
     expect(result).toMatchObject(
-      E.right({
-        statusCode: 500,
-        body: { status: 500, title: "AzureCosmosDB|db error" }
-      })
+      E.right(
+        H.problemJson({
+          status: 500,
+          title: "AzureCosmosDB|db error"
+        })
+      )
     );
   });
 
@@ -53,11 +56,12 @@ describe("Info handler", () => {
     })();
 
     expect(result).toMatchObject(
-      E.right({
-        statusCode: 200,
-        body: { name: "io-profile-async", version: expect.any(String) },
-        headers: expect.any(Object)
-      })
+      E.right(
+        H.successJson({
+          name: "io-profile-async",
+          version: expect.any(String)
+        })
+      )
     );
   });
 });
