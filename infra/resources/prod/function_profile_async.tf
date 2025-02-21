6a966b0bfa5cacd240340e970cdf0de20239cbb8
Gianmaria Scorza <gianmaria@scorza.dev>
2025-02-21 14:55:50 +0100

    backend internal url

--- infra/resources/prod/function_profile_async.tf
+++ infra/resources/prod/function_profile_async.tf
@@ -37,7 +37,7 @@ locals {
       MAIL_FROM = "IO - l'app dei servizi pubblici <no-reply@io.italia.it>"
 
       // Backend Internal
-      BACKEND_INTERNAL_BASE_URL = data.azurerm_app_service.app_backend_li.default_site_hostname
+      BACKEND_INTERNAL_BASE_URL = "https://${data.azurerm_app_service.app_backend_li.default_site_hostname}"
       BACKEND_INTERNAL_API_KEY  = data.azurerm_key_vault_secret.backendli_api_key.value
 
       // Function Profile
