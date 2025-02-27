67e031bf2118a477f69a04da011d5197b1a83310
Gianmaria Scorza <gianmaria@scorza.dev>
2025-02-27 15:52:29 +0100

    clean and revert

---
+++ apps/io-profile/src/__mocks__/env-config.mock.ts
@@ -0,0 +1,36 @@
+import { DateFromTimestamp } from "@pagopa/ts-commons/lib/dates";
+import { NonEmptyString } from "@pagopa/ts-commons/lib/strings";
+import { IConfig } from "../utils/config";
+
+export const envConfig = {
+  isProduction: false,
+
+  COSMOSDB_KEY: "aKey" as NonEmptyString,
+  COSMOSDB_NAME: "aName" as NonEmptyString,
+  COSMOSDB_URI: "aUri" as NonEmptyString,
+
+  FUNCTIONS_PUBLIC_URL: "aaa" as NonEmptyString,
+  MESSAGE_CONTAINER_NAME: "aaa" as NonEmptyString,
+
+  PUBLIC_API_KEY: "aaa" as NonEmptyString,
+  PUBLIC_API_URL: "aaa" as NonEmptyString,
+
+  EventsQueueStorageConnection: "aaa" as NonEmptyString,
+  EventsQueueName: "aaa" as NonEmptyString,
+  MIGRATE_SERVICES_PREFERENCES_PROFILE_QUEUE_NAME: "aaa" as NonEmptyString,
+  QueueStorageConnection: "aaa" as NonEmptyString,
+
+  SPID_LOGS_PUBLIC_KEY: "aaa" as NonEmptyString,
+  SUBSCRIPTIONS_FEED_TABLE: "aaa" as NonEmptyString,
+
+  OPT_OUT_EMAIL_SWITCH_DATE: "1577836800000" as unknown as DateFromTimestamp,
+
+  IS_CASHBACK_ENABLED: true,
+
+  // MailerConfig
+  MAIL_FROM: "aaa" as NonEmptyString,
+  MAILHOG_HOSTNAME: "aaa" as NonEmptyString,
+
+  NODE_ENV: "production",
+  REQ_SERVICE_ID: undefined,
+} as unknown as IConfig; // Not all the required envs are included in this mock
--- apps/io-profile/src/utils/__tests__/healthcheck.test.ts
+++ apps/io-profile/src/utils/__tests__/healthcheck.test.ts
@@ -3,9 +3,8 @@ import { beforeAll, describe, expect, it, vi } from "vitest";
 import { CosmosClient } from "@azure/cosmos";
 import * as E from "fp-ts/lib/Either";
 import { right } from "fp-ts/lib/Either";
+import { envConfig } from "../../__mocks__/env-config.mock";
 
-import { DateFromTimestamp } from "@pagopa/ts-commons/lib/dates";
-import { NonEmptyString } from "@pagopa/ts-commons/lib/strings";
 import * as config from "../config";
 
 import {
@@ -15,41 +14,7 @@ import {
   checkUrlHealth,
 } from "../healthcheck";
 
-import { IConfig } from "../config";
 import * as healthcheckUtils from "../healthcheck-utils";
-
-const envConfig = {
-  isProduction: false,
-
-  COSMOSDB_KEY: "aKey" as NonEmptyString,
-  COSMOSDB_NAME: "aName" as NonEmptyString,
-  COSMOSDB_URI: "aUri" as NonEmptyString,
-
-  FUNCTIONS_PUBLIC_URL: "aaa" as NonEmptyString,
-  MESSAGE_CONTAINER_NAME: "aaa" as NonEmptyString,
-
-  PUBLIC_API_KEY: "aaa" as NonEmptyString,
-  PUBLIC_API_URL: "aaa" as NonEmptyString,
-
-  EventsQueueStorageConnection: "aaa" as NonEmptyString,
-  EventsQueueName: "aaa" as NonEmptyString,
-  MIGRATE_SERVICES_PREFERENCES_PROFILE_QUEUE_NAME: "aaa" as NonEmptyString,
-  QueueStorageConnection: "aaa" as NonEmptyString,
-
-  SPID_LOGS_PUBLIC_KEY: "aaa" as NonEmptyString,
-  SUBSCRIPTIONS_FEED_TABLE: "aaa" as NonEmptyString,
-
-  OPT_OUT_EMAIL_SWITCH_DATE: "1577836800000" as unknown as DateFromTimestamp,
-
-  IS_CASHBACK_ENABLED: true,
-
-  // MailerConfig
-  MAIL_FROM: "aaa" as NonEmptyString,
-  MAILHOG_HOSTNAME: "aaa" as NonEmptyString,
-
-  NODE_ENV: "production",
-  REQ_SERVICE_ID: undefined,
-} as unknown as IConfig; // Not all the required envs are included in this mock
 
 const getBlobServiceKO = (name: string) => ({
   getServiceProperties: vi
