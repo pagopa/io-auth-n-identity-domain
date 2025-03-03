1d3c171b1654fd6f8bd47ad5fcc55cab603f03d8
gquadrati <greta.quadrati@pagopa.it>
2025-03-03 18:09:23 +0100

    fix: set function as disabled (prod+staging)

--- infra/resources/prod/function_profile_async.tf
+++ infra/resources/prod/function_profile_async.tf
@@ -101,7 +101,8 @@ module "function_profile_async" {
       AzureFunctionsJobHost__logging__applicationInsights__samplingSettings__minSamplingPercentage     = 5,
       AzureFunctionsJobHost__logging__applicationInsights__samplingSettings__maxSamplingPercentage     = 5,
       AzureFunctionsJobHost__logging__applicationInsights__samplingSettings__initialSamplingPercentage = 5,
-      "AzureWebJobs.ExpiredSessionAdvisor.Disabled"                                                    = "0"
+      "AzureWebJobs.ExpiredSessionAdvisor.Disabled"                                                    = "0",
+      "AzureWebJobs.MigrateServicePreferenceFromLegacy.Disabled"                                       = "1"
     }
   )
   slot_app_settings = merge(
@@ -110,7 +111,8 @@ module "function_profile_async" {
       AzureFunctionsJobHost__logging__applicationInsights__samplingSettings__minSamplingPercentage     = 100,
       AzureFunctionsJobHost__logging__applicationInsights__samplingSettings__maxSamplingPercentage     = 100,
       AzureFunctionsJobHost__logging__applicationInsights__samplingSettings__initialSamplingPercentage = 100,
-      "AzureWebJobs.ExpiredSessionAdvisor.Disabled"                                                    = "1"
+      "AzureWebJobs.ExpiredSessionAdvisor.Disabled"                                                    = "1",
+      "AzureWebJobs.MigrateServicePreferenceFromLegacy.Disabled"                                       = "1"
     }
   )
 
@@ -118,7 +120,8 @@ module "function_profile_async" {
     "AzureFunctionsJobHost__logging__applicationInsights__samplingSettings__minSamplingPercentage",
     "AzureFunctionsJobHost__logging__applicationInsights__samplingSettings__maxSamplingPercentage",
     "AzureFunctionsJobHost__logging__applicationInsights__samplingSettings__initialSamplingPercentage",
-    "AzureWebJobs.ExpiredSessionAdvisor.Disabled"
+    "AzureWebJobs.ExpiredSessionAdvisor.Disabled",
+    "AzureWebJobs.MigrateServicePreferenceFromLegacy.Disabled"
   ]
 
   subnet_service_endpoints = {
