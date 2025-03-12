8e74ecd2019f9b87c91f752fbe0b117609547abf
Gianmaria Scorza <gianmaria@scorza.dev>
2025-03-12 15:44:05 +0100

    cleanUp

--- infra/resources/prod/function_profile.tf
+++ infra/resources/prod/function_profile.tf
@@ -166,11 +166,7 @@ module "function_profile" {
     {
       AzureFunctionsJobHost__logging__applicationInsights__samplingSettings__minSamplingPercentage     = 5,
       AzureFunctionsJobHost__logging__applicationInsights__samplingSettings__maxSamplingPercentage     = 5,
-      AzureFunctionsJobHost__logging__applicationInsights__samplingSettings__initialSamplingPercentage = 5,
-      "AzureWebJobs.ExpiredSessionAdvisor.Disabled"                                                    = "0",
-      "AzureWebJobs.MigrateServicePreferenceFromLegacy.Disabled"                                       = "0",
-      "AzureWebJobs.OnProfileUpdate.Disabled"                                                          = "0"
-      "AzureWebJobs.StoreSpidLogs.Disabled"                                                            = "0"
+      AzureFunctionsJobHost__logging__applicationInsights__samplingSettings__initialSamplingPercentage = 5
     }
   )
   slot_app_settings = merge(
@@ -178,22 +174,14 @@ module "function_profile" {
     {
       AzureFunctionsJobHost__logging__applicationInsights__samplingSettings__minSamplingPercentage     = 100,
       AzureFunctionsJobHost__logging__applicationInsights__samplingSettings__maxSamplingPercentage     = 100,
-      AzureFunctionsJobHost__logging__applicationInsights__samplingSettings__initialSamplingPercentage = 100,
-      "AzureWebJobs.ExpiredSessionAdvisor.Disabled"                                                    = "1",
-      "AzureWebJobs.MigrateServicePreferenceFromLegacy.Disabled"                                       = "1",
-      "AzureWebJobs.OnProfileUpdate.Disabled"                                                          = "1"
-      "AzureWebJobs.StoreSpidLogs.Disabled"                                                            = "1"
+      AzureFunctionsJobHost__logging__applicationInsights__samplingSettings__initialSamplingPercentage = 100
     }
   )
 
   sticky_app_setting_names = [
     "AzureFunctionsJobHost__logging__applicationInsights__samplingSettings__minSamplingPercentage",
     "AzureFunctionsJobHost__logging__applicationInsights__samplingSettings__maxSamplingPercentage",
-    "AzureFunctionsJobHost__logging__applicationInsights__samplingSettings__initialSamplingPercentage",
-    "AzureWebJobs.ExpiredSessionAdvisor.Disabled",
-    "AzureWebJobs.MigrateServicePreferenceFromLegacy.Disabled",
-    "AzureWebJobs.OnProfileUpdate.Disabled",
-    "AzureWebJobs.StoreSpidLogs.Disabled"
+    "AzureFunctionsJobHost__logging__applicationInsights__samplingSettings__initialSamplingPercentage"
   ]
 
   subnet_service_endpoints = {
