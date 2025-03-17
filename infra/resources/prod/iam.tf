1c33f25247129b5510bed7da3b0d0fc59d4e5a0c
Krusty93 <andrea.grillo@pagopa.it>
2025-03-17 10:49:27 +0100

    remove identity and docs

--- infra/resources/prod/iam.tf
+++ infra/resources/prod/iam.tf
@@ -153,21 +153,21 @@ resource "azurerm_role_assignment" "devs_group_rg_shared" {
 #########################
 
 resource "azurerm_role_assignment" "infra_cd_rg_elt_contributor" {
-  scope                = azurerm_resource_group.rg_elt .id
+  scope                = azurerm_resource_group.rg_elt.id
   role_definition_name = "Contributor"
   principal_id         = data.azurerm_user_assigned_identity.infra_cd_01.principal_id
   description          = "Allow Infra CD identity to apply changes to resources at resource group scope"
 }
 
 resource "azurerm_role_assignment" "admins_group_rg_elt" {
-  scope                = azurerm_resource_group.rg_elt .id
+  scope                = azurerm_resource_group.rg_elt.id
   role_definition_name = "Owner"
   principal_id         = data.azuread_group.auth_admins.object_id
   description          = "Allow AD Admin group the complete ownership at resource group scope"
 }
 
 resource "azurerm_role_assignment" "devs_group_rg_elt" {
-  scope                = azurerm_resource_group.rg_elt .id
+  scope                = azurerm_resource_group.rg_elt.id
   role_definition_name = "Contributor"
   principal_id         = data.azuread_group.auth_devs.object_id
   description          = "Allow AD Dev group to apply changes at resource group scope"
