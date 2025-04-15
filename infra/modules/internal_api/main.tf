14760365b3343cbb41422cd59a4dcd489c95695e
salvatorelaiso <salvatorelaiso@gmail.com>
2025-04-15 17:50:40 +0200

    fix: improve description

--- infra/modules/internal_api/main.tf
+++ infra/modules/internal_api/main.tf
@@ -5,7 +5,7 @@ module "apim_v2_product_auth-n-identity_internal" {
   api_management_name   = var.apim_name
   resource_group_name   = var.apim_resource_group_name
   display_name          = "IO AUTH AND IDENTITY INTERNAL API"
-  description           = "Product for IO Auth And Identity APIs."
+  description           = "Product for IO Auth And Identity APIs exposed for internal consumption by other domains."
   subscription_required = true
   approval_required     = false
   published             = true
