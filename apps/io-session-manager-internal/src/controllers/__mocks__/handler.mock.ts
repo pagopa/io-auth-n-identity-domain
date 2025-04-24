e01245b5640769b85a40a1aa36c404e2d33609b0
salvatorelaiso <salvatorelaiso@gmail.com>
2025-04-24 15:48:28 +0200

    fix: type

--- apps/io-session-manager-internal/src/controllers/__mocks__/handler.mock.ts
+++ apps/io-session-manager-internal/src/controllers/__mocks__/handler.mock.ts
@@ -2,7 +2,7 @@ import * as H from "@pagopa/handler-kit";
 import * as L from "@pagopa/logger";
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-export const httpHandlerInputMocks: H.HandlerEnvironment<any> = {
+export const httpHandlerInputMocks: H.HandlerEnvironment<H.HttpRequest> = {
   input: H.request("mockurl"),
   inputDecoder: H.HttpRequest,
   logger: {
