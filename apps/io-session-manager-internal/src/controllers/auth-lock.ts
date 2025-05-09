505410a9218fbe0ff30e1bc62ad1d12a887f5386
arcogabbo <arcogabbo@gmail.com>
2025-05-09 11:51:20 +0200

    added AuthLock endpoint and dependencies

---
+++ apps/io-session-manager-internal/src/controllers/auth-lock.ts
@@ -0,0 +1,70 @@
+import { httpAzureFunction } from "@pagopa/handler-kit-azure-func";
+import * as H from "@pagopa/handler-kit";
+import * as TE from "fp-ts/lib/TaskEither";
+import * as RTE from "fp-ts/ReaderTaskEither";
+import { pipe } from "fp-ts/lib/function";
+import { FiscalCode, NonEmptyString } from "@pagopa/ts-commons/lib/strings";
+import { sequenceS } from "fp-ts/lib/Apply";
+import {
+  LockUserAuthenticationDeps,
+  SessionService,
+} from "../services/session-service";
+import { RequiredPathParamMiddleware } from "../utils/middlewares/required-path-param";
+import { DomainErrorTypes } from "../utils/errors";
+import { RequiredBodyMiddleware } from "../utils/middlewares/required-body";
+import { AuthLockBody } from "../generated/definitions/internal/AuthLockBody";
+
+type Dependencies = {
+  SessionService: SessionService;
+};
+
+const lockUserAuthentication: (
+  fiscalCode: FiscalCode,
+  authLockBody: AuthLockBody,
+) => RTE.ReaderTaskEither<
+  Dependencies & LockUserAuthenticationDeps,
+  H.HttpError | H.HttpConflictError,
+  H.HttpResponse<null, 204>
+> = (fiscalCode, authLockBody) => (deps) =>
+  pipe(
+    deps.SessionService.lockUserAuthentication(
+      fiscalCode,
+      authLockBody.unlock_code,
+    )(deps),
+    TE.map(() => H.withStatusCode(204)(H.success(null))),
+    TE.mapLeft((error) => {
+      switch (error.kind) {
+        case DomainErrorTypes.GENERIC_ERROR:
+          return new H.HttpError(error.causedBy?.message);
+        case DomainErrorTypes.CONFLICT:
+          return new H.HttpConflictError();
+      }
+    }),
+  );
+
+export const makeAuthLockHandler: H.Handler<
+  H.HttpRequest,
+  | H.HttpResponse<null, 204>
+  | H.HttpResponse<H.ProblemJson, H.HttpErrorStatusCode>,
+  Dependencies & LockUserAuthenticationDeps
+> = H.of((req: H.HttpRequest) =>
+  pipe(
+    req,
+    sequenceS(RTE.ApplyPar)({
+      fiscalCode: RequiredPathParamMiddleware(
+        FiscalCode,
+        "fiscalCode" as NonEmptyString,
+      ),
+      authLockBody: RequiredBodyMiddleware(AuthLockBody),
+    }),
+    RTE.fromTaskEither,
+    RTE.chain(({ fiscalCode, authLockBody }) =>
+      lockUserAuthentication(fiscalCode, authLockBody),
+    ),
+    RTE.orElseW((error) =>
+      RTE.right(H.problemJson({ status: error.status, title: error.message })),
+    ),
+  ),
+);
+
+export const AuthLockFunction = httpAzureFunction(makeAuthLockHandler);
---
+++ apps/io-session-manager-internal/src/repositories/lollipop.ts
@@ -0,0 +1,44 @@
+import { QueueClient, QueueSendMessageResponse } from "@azure/storage-queue";
+import * as RTE from "fp-ts/ReaderTaskEither";
+import * as TE from "fp-ts/lib/TaskEither";
+import { constVoid, pipe } from "fp-ts/lib/function";
+import * as E from "fp-ts/lib/Either";
+import { RevokeAssertionRefInfo } from "@pagopa/io-functions-commons/dist/src/entities/revoke_assertion_ref_info";
+import { Reader } from "fp-ts/lib/Reader";
+import { AssertionRef } from "../generated/definitions/internal/AssertionRef";
+import { base64EncodeObject } from "../utils/encoding";
+
+type Dependencies = {
+  RevokeAssertionRefQueueClient: QueueClient;
+};
+
+const revokePreviousAssertionRef: (
+  assertionRef: AssertionRef,
+) => Reader<Dependencies, Promise<QueueSendMessageResponse>> =
+  (assertionRef) => (deps) => {
+    const revokeMessage = RevokeAssertionRefInfo.encode({
+      assertion_ref: assertionRef,
+    });
+    return deps.RevokeAssertionRefQueueClient.sendMessage(
+      base64EncodeObject(revokeMessage),
+    );
+  };
+
+const fireAndForgetRevokeAssertionRef: (
+  assertionRef: AssertionRef,
+) => RTE.ReaderTaskEither<Dependencies, Error, true> =
+  (assertionRef) => (deps) =>
+    pipe(
+      TE.tryCatch(
+        () =>
+          // fire and forget the queue message
+          new Promise<true>((resolve) => {
+            revokePreviousAssertionRef(assertionRef)(deps).catch(constVoid);
+            resolve(true);
+          }),
+        E.toError,
+      ),
+    );
+
+export type LollipopRepository = typeof LollipopRepository;
+export const LollipopRepository = { fireAndForgetRevokeAssertionRef };
