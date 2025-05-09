ce78532d02efaf09b9f54b9956191270dff63abe
arcogabbo <arcogabbo@gmail.com>
2025-05-09 11:51:20 +0200

    added AuthLock endpoint and dependencies

--- apps/io-session-manager-internal/src/controllers/main.ts
+++ apps/io-session-manager-internal/src/controllers/main.ts
@@ -1,4 +1,6 @@
 import { app } from "@azure/functions";
+import { TableClient } from "@azure/data-tables";
+import { QueueClient } from "@azure/storage-queue";
 import { InfoService } from "../services/info";
 import { Package } from "../repositories/package";
 import { CreateRedisClientSingleton } from "../utils/redis-client";
@@ -6,8 +8,12 @@ import { getConfigOrThrow } from "../utils/config";
 import { SessionService } from "../services/session-service";
 import { RedisRepository } from "../repositories/redis";
 import { initTelemetryClient } from "../utils/appinsights";
+import { SessionLockRepository } from "../repositories/session-lock";
+import { InstallationRepository } from "../repositories/installation";
+import { LollipopRepository } from "../repositories/lollipop";
 import { InfoFunction } from "./info";
 import { GetSessionFunction } from "./get-session";
+import { AuthLockFunction } from "./auth-lock";
 
 const v1BasePath = "api/v1";
 const config = getConfigOrThrow();
@@ -27,6 +33,21 @@ const safeRedisClientTask = CreateRedisClientSingleton(
   appInsightsTelemetryClient,
 );
 
+const AuthenticationLockTableClient = TableClient.fromConnectionString(
+  config.LOCKED_PROFILES_STORAGE_CONNECTION_STRING,
+  config.LOCKED_PROFILES_TABLE_NAME,
+);
+
+const RevokeAssertionRefQueueClient = new QueueClient(
+  config.LOLLIPOP_REVOKE_STORAGE_CONNECTION_STRING,
+  config.LOLLIPOP_REVOKE_QUEUE_NAME,
+);
+
+const NotificationQueueClient = new QueueClient(
+  config.PUSH_NOTIFICATIONS_STORAGE_CONNECTION_STRING,
+  config.PUSH_NOTIFICATIONS_QUEUE_NAME,
+);
+
 app.http("Info", {
   authLevel: "anonymous",
   handler: InfoFunction({ InfoService, Package }),
@@ -44,4 +65,22 @@ app.http("GetSession", {
   }),
   methods: ["GET"],
   route: `${v1BasePath}/sessions/{fiscalCode}`,
+});
+
+app.http("AuthLock", {
+  authLevel: "function",
+  handler: AuthLockFunction({
+    FastRedisClientTask: fastRedisClientTask,
+    SafeRedisClientTask: safeRedisClientTask,
+    SessionService,
+    RedisRepository,
+    SessionLockRepository,
+    InstallationRepository,
+    LollipopRepository,
+    NotificationQueueClient,
+    AuthenticationLockTableClient,
+    RevokeAssertionRefQueueClient,
+  }),
+  methods: ["POST"],
+  route: `${v1BasePath}/auth/{fiscalCode}/lock`,
 });
