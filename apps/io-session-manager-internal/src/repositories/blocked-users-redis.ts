282bef397a286fb47cba48924fd1ef5cf428aeaa
gquadrati <greta.quadrati@pagopa.it>
2025-05-13 12:21:13 +0200

    cleanup duplicated code

--- apps/io-session-manager-internal/src/repositories/blocked-users-redis.ts
+++ apps/io-session-manager-internal/src/repositories/blocked-users-redis.ts
@@ -4,40 +4,16 @@ import * as RTE from "fp-ts/lib/ReaderTaskEither";
 import * as TE from "fp-ts/lib/TaskEither";
 import * as E from "fp-ts/lib/Either";
 import { pipe } from "fp-ts/lib/function";
-import { isNumber } from "fp-ts/lib/number";
 
 import { FiscalCode } from "@pagopa/ts-commons/lib/strings";
+
+import { falsyResponseToErrorAsync, integerReplyAsync } from "./redis";
 
 type FastRedisClientDependency = { fastClient: redisLib.RedisClusterType };
 type SafeRedisClientDependency = { safeClient: redisLib.RedisClusterType };
 type Dependencies = FastRedisClientDependency & SafeRedisClientDependency;
 
 const blockedUserSetKey = "BLOCKEDUSERS";
-
-// -----------------------
-// Private functions
-// -----------------------
-
-const integerReplyAsync =
-  (expectedReply?: number) =>
-  (command: TE.TaskEither<Error, unknown>): TE.TaskEither<Error, boolean> =>
-    pipe(
-      command,
-      TE.chain((reply) => {
-        if (expectedReply !== undefined && expectedReply !== reply) {
-          return TE.right(false);
-        }
-        return TE.right(isNumber(reply));
-      }),
-    );
-
-const falsyResponseToErrorAsync =
-  (error: Error) =>
-  (response: TE.TaskEither<Error, boolean>): TE.TaskEither<Error, true> =>
-    pipe(
-      response,
-      TE.chain((_) => (_ ? TE.right(_) : TE.left(error))),
-    );
 
 // -----------------------
 // Public functions
