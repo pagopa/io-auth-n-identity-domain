19717a780240449d37cffd493120ea095186f46a
salvatorelaiso <salvatorelaiso@gmail.com>
2025-05-15 12:11:27 +0200

    feat: interval from date

--- apps/io-profile-async/src/types/interval.ts
+++ apps/io-profile-async/src/types/interval.ts
@@ -7,3 +7,15 @@ export const Interval = t.type({
 });
 
 export type Interval = t.TypeOf<typeof Interval>;
+
+/**
+ * Returns an Interval from midnight of the given date to midnight of the next day.
+ *
+ * @param date The date to create the interval from
+ * @returns An Interval object with the start and end dates
+ */
+export const createInterval = (date: Date): Interval => {
+  const from = new Date(date.setHours(0, 0, 0, 0));
+  const to = new Date(date.setHours(24, 0, 0, 0));
+  return { from, to };
+};
