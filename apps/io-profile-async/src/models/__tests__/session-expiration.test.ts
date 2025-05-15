85cbceb9faba7302507b9799cd36546966f27b94
salvatorelaiso <salvatorelaiso@gmail.com>
2025-05-15 14:50:51 +0200

    feat: `SessionExpirationModel`

---
+++ apps/io-profile-async/src/models/__tests__/session-expiration.test.ts
@@ -0,0 +1,114 @@
+import { beforeEach, describe, it, expect, vi } from "vitest";
+import { Container } from "@azure/cosmos";
+import { NonEmptyString } from "@pagopa/ts-commons/lib/strings";
+import * as E from "fp-ts/lib/Either";
+import * as t from "io-ts";
+import {
+  NotificationEvents,
+  RetrievedSessionExpiration,
+  SessionExpiration,
+  SessionExpirationModel
+} from "../session-expiration";
+
+const anId = "AAAAAA89S20I111X" as NonEmptyString;
+const anExpirationDate = 1746992855578;
+const aTtl = 885551;
+
+const aNotificationEvents = {
+  EXPIRING_SESSION: false,
+  EXPIRED_SESSION: true
+} as NotificationEvents;
+
+const aSessionExpiration = {
+  id: anId,
+  expirationDate: anExpirationDate,
+  notificationEvents: aNotificationEvents,
+  ttl: aTtl
+} as SessionExpiration;
+
+const cosmosMetadata = {
+  _rid: "rid",
+  _self: "self",
+  _etag: "etag",
+  _ts: 1693992855
+};
+
+const aRetrievedSessionExpiration = {
+  ...aSessionExpiration,
+  ...cosmosMetadata,
+  kind: "IRetrievedSessionExpiration"
+} as RetrievedSessionExpiration;
+
+describe("SessionExpiration model decoding", () => {
+  it("should decode a SessionExpiration", () => {
+    const sessionExpiration = SessionExpiration.decode(aSessionExpiration);
+
+    expect(sessionExpiration).toEqual(
+      E.right({
+        ...aSessionExpiration
+      })
+    );
+  });
+
+  it("should decode a RetrievedSessionExpiration as a SessionExpiration", () => {
+    const sessionExpiration = SessionExpiration.decode(
+      aRetrievedSessionExpiration
+    );
+
+    // It keeps the data of the original object but it doesn't fail the decoding
+    expect(sessionExpiration).toEqual(
+      E.right({
+        ...aRetrievedSessionExpiration
+      })
+    );
+  });
+});
+
+describe("RetrievedSessionExpiration model decoding", () => {
+  it("should decode a RetrievedSessionExpiration", () => {
+    const retrievedSessionExpiration = RetrievedSessionExpiration.decode(
+      aRetrievedSessionExpiration
+    );
+
+    expect(retrievedSessionExpiration).toEqual(
+      E.right({
+        ...aRetrievedSessionExpiration
+      })
+    );
+  });
+
+  it("should fail to decode a SessionExpiration as RetrievedSessionExpiration", () => {
+    const invalid = RetrievedSessionExpiration.decode(aSessionExpiration);
+    expect(E.isLeft(invalid)).toBe(true);
+  });
+});
+
+describe("buildAsyncIterable", () => {
+  const containerMock: Container = {} as Container;
+  const model: SessionExpirationModel = new SessionExpirationModel(
+    containerMock
+  );
+
+  beforeEach(() => {
+    vi.clearAllMocks();
+  });
+
+  it("should call getQueryIterator with correct parameters", async () => {
+    const query = "SELECT * FROM c";
+    const chunkSize = 10;
+    const expectedResult = {} as AsyncIterable<
+      ReadonlyArray<t.Validation<RetrievedSessionExpiration>>
+    >;
+
+    const getQueryIteratorSpy = vi
+      .spyOn(model, "getQueryIterator")
+      .mockReturnValue(expectedResult);
+
+    const result = model.buildAsyncIterable(query, chunkSize);
+
+    expect(getQueryIteratorSpy).toHaveBeenCalledWith(query, {
+      maxItemCount: chunkSize
+    });
+    expect(result).toBe(expectedResult);
+  });
+});
---
+++ apps/io-profile-async/src/models/session-expiration.ts
@@ -0,0 +1,68 @@
+import { Container, SqlQuerySpec } from "@azure/cosmos";
+import {
+  BaseModelTTL,
+  CosmosdbModelTTL,
+  CosmosResourceTTL
+} from "@pagopa/io-functions-commons/dist/src/utils/cosmosdb_model_ttl";
+import { wrapWithKind } from "@pagopa/io-functions-commons/dist/src/utils/types";
+import * as t from "io-ts";
+
+export const SESSION_EXPIRATION_MODEL_KEY_FIELD = "id";
+export const SESSION_EXPIRATION_ROW_PK_FIELD = "expirationDate";
+
+export const NotificationEvents = t.partial({
+  EXPIRED_SESSION: t.boolean,
+  EXPIRING_SESSION: t.boolean
+});
+
+export type NotificationEvents = t.TypeOf<typeof NotificationEvents>;
+
+export const SessionExpiration = t.type({
+  id: t.string,
+  expirationDate: t.number,
+  notificationEvents: NotificationEvents
+});
+
+export type SessionExpiration = t.TypeOf<typeof SessionExpiration>;
+
+export const NewSessionExpiration = wrapWithKind(
+  t.intersection([SessionExpiration, BaseModelTTL]),
+  "INewSessionExpiration" as const
+);
+export type NewSessionExpiration = t.TypeOf<typeof NewSessionExpiration>;
+
+export const RetrievedSessionExpiration = wrapWithKind(
+  t.intersection([SessionExpiration, CosmosResourceTTL]),
+  "IRetrievedSessionExpiration" as const
+);
+export type RetrievedSessionExpiration = t.TypeOf<
+  typeof RetrievedSessionExpiration
+>;
+
+export class SessionExpirationModel extends CosmosdbModelTTL<
+  SessionExpiration,
+  NewSessionExpiration,
+  RetrievedSessionExpiration,
+  typeof SESSION_EXPIRATION_MODEL_KEY_FIELD,
+  typeof SESSION_EXPIRATION_ROW_PK_FIELD
+> {
+  constructor(container: Container) {
+    super(container, NewSessionExpiration, RetrievedSessionExpiration);
+  }
+
+  /**
+   * Returns an async iterable of session expiration objects
+   *
+   * @param query - The SQL query to execute
+   * @param cosmosChunkSize - The maximum number of items to return in each page
+   * @returns An async iterable of session expiration objects
+   */
+  public buildAsyncIterable(
+    query: string | SqlQuerySpec,
+    cosmosChunkSize: number
+  ): AsyncIterable<ReadonlyArray<t.Validation<RetrievedSessionExpiration>>> {
+    return this.getQueryIterator(query, {
+      maxItemCount: cosmosChunkSize
+    });
+  }
+}
