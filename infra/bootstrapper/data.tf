4a74d2bace3614f7891f58eb03101fda65510206
Andrea Grillo <andrea.grillo@pagopa.it>
2025-05-26 12:58:41 +0200

    [CES-586] Add Slack Webhook URL (#313)
    <!--- Please always add a PR description as if nobody knows anything
    about the context these changes come from. -->
    <!--- Even if we are all from our internal team, we may not be on the
    same page. -->
    <!--- Write this PR as you were contributing to a public OSS project,
    where nobody knows you and you have to earn their trust. -->
    <!--- This will improve our projects in the long run! Thanks. -->
    
    #### List of Changes
    <!--- Describe your changes in detail -->
    
    Add to repository the slack webhook URL to get notified about drifts in
    Terraform configuration
    
    #### Motivation and Context
    <!--- Why is this change required? What problem does it solve? -->
    
    Monitoring infrastructure
    
    Close CES-586
    
    #### How Has This Been Tested?
    <!--- Please describe in detail how you tested your changes. -->
    <!--- Include details of your testing environment, tests ran to see how
    -->
    <!--- your change affects other areas of the code, etc. -->
    
    #### Screenshots (if appropriate):
    
    #### Types of changes
    <!--- What types of changes does your code introduce? Put an `x` in all
    the boxes that apply: -->
    - [ ] Chore (nothing changes by a user perspective)
    - [ ] Bug fix (non-breaking change which fixes an issue)
    - [ ] New feature (non-breaking change which adds functionality)
    - [ ] Breaking change (fix or feature that would cause existing
    functionality to not work as expected)
    
    #### Checklist:
    <!--- Go over all the following points, and put an `x` in all the boxes
    that apply. -->
    <!--- If you're unsure about any of these, don't hesitate to ask. We're
    here to help! -->
    - [ ] My change requires a changeset, which I've added.
    - [ ] My change requires a change to the documentation.
    - [ ] I have updated the documentation accordingly.

--- infra/bootstrapper/data.tf
+++ infra/bootstrapper/data.tf
@@ -7,3 +7,8 @@ data "azurerm_key_vault_secret" "sonacloud_token" {
   key_vault_id = data.azurerm_key_vault.citizen_auth_kv.id
   name         = "session-manager-repo-sonarcloud-token"
 }
+
+data "azurerm_key_vault_secret" "slack_webhook_url" {
+  key_vault_id = data.azurerm_key_vault.citizen_auth_kv.id
+  name         = "slack-webhook-url-terraform-drift"
+}
