d5472ab815a2514d921dbd6430ad1cf781cddc76
salvatorelaiso <salvatorelaiso@gmail.com>
2025-05-28 09:41:27 +0200

    fix: revert `BackendInternalConfig` removal

--- apps/io-fast-login/src/config.ts
+++ apps/io-fast-login/src/config.ts
@@ -28,6 +28,12 @@ const GetAssertionConfig = t.type(
 );
 type GetAssertionConfig = t.Type<typeof GetAssertionConfig>;
 
+const BackendInternalConfig = t.type({
+  BACKEND_INTERNAL_API_KEY: NonEmptyString,
+  BACKEND_INTERNAL_BASE_URL: UrlFromString
+});
+type BackendInternalConfig = t.TypeOf<typeof BackendInternalConfig>;
+
 const SessionManagerInternalConfig = t.type({
   SESSION_MANAGER_INTERNAL_API_KEY: NonEmptyString,
   SESSION_MANAGER_INTERNAL_BASE_URL: UrlFromString
@@ -60,6 +66,7 @@ export const IConfig = t.intersection([
 
     isProduction: t.boolean
   }),
+  BackendInternalConfig,
   SessionManagerInternalConfig,
   GetAssertionConfig,
   RedisClientConfig
