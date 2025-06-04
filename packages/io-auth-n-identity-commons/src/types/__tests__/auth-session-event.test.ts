8c62d4684886070e24e1dd7f9302ee6834259155
Gianmaria Scorza <gianmaria@scorza.dev>
2025-06-04 14:23:30 +0200

    bootstrap team common package

---
+++ packages/io-auth-n-identity-commons/src/types/__tests__/auth-session-event.test.ts
@@ -0,0 +1,60 @@
+import * as E from "fp-ts/lib/Either";
+import { describe, expect, it } from "vitest";
+import { AuthSessionEvent } from "../auth-session-event";
+
+describe("AuthSessionEvent decode tests", () => {
+  it("should decode a valid Login event", () => {
+    const aValidLoginEvent = {
+      eventType: "login",
+      fiscalCode: "VSFNVG14A39Y596X",
+      expiredAt: 1748508155
+    };
+    const decodeResult = AuthSessionEvent.decode(aValidLoginEvent);
+
+    expect(decodeResult).toStrictEqual(
+      E.of({
+        ...aValidLoginEvent,
+        expiredAt: new Date(aValidLoginEvent.expiredAt)
+      })
+    );
+  });
+
+  it("should fail when a login event lack of required properties", () => {
+    const aBadLoginEvent = {
+      eventType: "login",
+      fiscalCode: "VSFNVG14A39Y596X"
+    };
+    const decodeResult = AuthSessionEvent.decode(aBadLoginEvent);
+
+    expect(E.isLeft(decodeResult)).toBe(true);
+  });
+
+  it("should decode a valid Logout event", () => {
+    const aValidLogoutEvent = {
+      eventType: "logout",
+      fiscalCode: "VSFNVG14A39Y596X"
+    };
+    const decodeResult = AuthSessionEvent.decode(aValidLogoutEvent);
+
+    expect(decodeResult).toStrictEqual(E.of(aValidLogoutEvent));
+  });
+
+  it("should fail when a logout event lack of required properties", () => {
+    const aBadLogoutEvent = {
+      eventType: "logout"
+    };
+    const decodeResult = AuthSessionEvent.decode(aBadLogoutEvent);
+
+    expect(E.isLeft(decodeResult)).toBe(true);
+  });
+
+  it("should fail when an unknown event is given", () => {
+    const anUnknownEvent = {
+      eventType: "unknownEvent",
+      fiscalCode: "VSFNVG14A39Y596X"
+    };
+    const decodeResult = AuthSessionEvent.decode(anUnknownEvent);
+
+    expect(E.isLeft(decodeResult)).toBe(true);
+  });
+});
---
+++ packages/io-auth-n-identity-commons/src/types/auth-session-event.ts
@@ -0,0 +1,35 @@
+import { DateFromTimestamp } from "@pagopa/ts-commons/lib/dates";
+import { FiscalCode } from "@pagopa/ts-commons/lib/strings";
+import { enumType } from "@pagopa/ts-commons/lib/types";
+import * as t from "io-ts";
+
+export enum EventTypeEunum {
+  "LOGIN" = "login",
+  "LOGOUT" = "logout"
+}
+
+export type EventType = t.TypeOf<typeof EventType>;
+export const EventType = enumType<EventTypeEunum>(EventTypeEunum, "EventType");
+
+export const BaseAuthSessionEvent = t.type({
+  fiscalCode: FiscalCode
+});
+
+export const LoginEvent = t.intersection([
+  t.type({
+    expiredAt: DateFromTimestamp
+  }),
+  t.type({ eventType: t.literal(EventTypeEunum.LOGIN) }),
+  BaseAuthSessionEvent
+]);
+export type LoginEvent = t.TypeOf<typeof LoginEvent>;
+
+//TODO: this will contain more data, under definition what to include in this event
+export const LogoutEvent = t.intersection([
+  t.type({ eventType: t.literal(EventTypeEunum.LOGOUT) }),
+  BaseAuthSessionEvent
+]);
+export type LogoutEvent = t.TypeOf<typeof LogoutEvent>;
+
+export const AuthSessionEvent = t.union([LoginEvent, LogoutEvent]);
+export type AuthSessionEvent = t.TypeOf<typeof AuthSessionEvent>;
