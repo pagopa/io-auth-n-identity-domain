2226eae5645530ebfe3e25716b0a9faa000aff08
Gianmaria Scorza <gianmaria@scorza.dev>
2025-06-09 17:25:18 +0200

    Add findByFiscalCodeAsyncIterable and deleteRecord function in Session Notifications Repository

--- apps/io-profile-async/src/repositories/session-notifications.ts
+++ apps/io-profile-async/src/repositories/session-notifications.ts
@@ -7,6 +7,7 @@ import * as TE from "fp-ts/TaskEither";
 import * as t from "io-ts";
 import {
   RetrievedSessionNotifications,
+  SESSION_NOTIFICATIONS_MODEL_KEY_FIELD,
   SESSION_NOTIFICATIONS_ROW_PK_FIELD,
   SessionNotificationsModel
 } from "../models/session-notifications";
@@ -48,6 +49,42 @@ const findByExpiredAtAsyncIterable: (
     chunkSize
   );
 
+// TODO: Add Tests
+/**
+ * Finds session-notifications documents by fiscalCode.
+ *
+ * @param interval The interval to search for
+ * @returns A TaskEither that resolves to an array of all session-notifications documents having the given fiscalCode
+ */
+const findByFiscalCodeAsyncIterable: (
+  fiscalCode: FiscalCode,
+  chunkSize: number
+) => R.Reader<
+  Dependencies,
+  AsyncIterable<ReadonlyArray<t.Validation<RetrievedSessionNotifications>>>
+> = (fiscalCode, chunkSize) => deps =>
+  deps.sessionNotificationsModel.buildAsyncIterable(
+    {
+      parameters: [
+        {
+          name: "@fiscalCode",
+          value: fiscalCode
+        }
+      ],
+      query: `SELECT * FROM c WHERE c.${SESSION_NOTIFICATIONS_MODEL_KEY_FIELD} = @fiscalCode)`
+    },
+    chunkSize
+  );
+
+// TODO: Add Tests
+const deleteRecord: (
+  fiscalCode: FiscalCode,
+  expiredAt: number
+) => RTE.ReaderTaskEither<Dependencies, CosmosErrors, void> = (
+  fiscalCode,
+  expiredAt
+) => deps => deps.sessionNotificationsModel.delete([fiscalCode, expiredAt]);
+
 /**
  * Updates notification events for a session-notifications document.
  *
@@ -76,6 +113,8 @@ const updateExpiredSessionNotificationFlag: (
 
 export type SessionNotificationsRepository = typeof SessionNotificationsRepository;
 export const SessionNotificationsRepository = {
+  deleteRecord,
   findByExpiredAtAsyncIterable,
+  findByFiscalCodeAsyncIterable,
   updateExpiredSessionNotificationFlag
 };
