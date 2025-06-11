d67774092c68eda6520796d37af670866789b9ec
Gianmaria Scorza <gianmaria@scorza.dev>
2025-06-11 18:06:06 +0200

    better validationErrorsContainsKnownEventType implementation

--- packages/io-auth-n-identity-commons/src/utils/auth-session-event-utils.ts
+++ packages/io-auth-n-identity-commons/src/utils/auth-session-event-utils.ts
@@ -1,20 +1,20 @@
 import * as E from "fp-ts/lib/Either";
-import { pipe } from "fp-ts/lib/function";
+import { flow, pipe } from "fp-ts/lib/function";
 import * as O from "fp-ts/Option";
+import * as t from "io-ts";
 import { Errors } from "io-ts";
 import { EventType } from "../types/auth-session-event";
+
+const WithKnownEventType = t.type({
+  eventType: EventType,
+});
+type WithKnownEventType = t.TypeOf<typeof WithKnownEventType>;
 
 export const validationErrorsContainsKnownEventType = (
   validationErrors: Errors,
 ): boolean =>
   pipe(
-    validationErrors,
-    O.fromPredicate((e) => e.length > 0),
-    O.chain((e) => O.tryCatch(() => e[0].context[0].actual)),
-    O.filter(
-      (u): u is { eventType: unknown } =>
-        typeof u === "object" && u !== null && "eventType" in u,
-    ),
-    O.map((u) => pipe(u.eventType, EventType.decode, E.isRight)),
+    O.tryCatch(() => validationErrors[0].context[0].actual),
+    O.map(flow(WithKnownEventType.decode, E.isRight)),
     O.getOrElse(() => false),
   );
