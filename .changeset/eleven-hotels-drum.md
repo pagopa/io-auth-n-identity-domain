1cbfc9540c7d08b6ecb9dd651bc81e035688b093
Salvatore Laiso <32564922+salvatorelaiso@users.noreply.github.com>
2025-06-11 12:42:09 +0200

    Fix typo in `EventTypeEnum` declaration (#339)
    <!--- Please always add a PR description as if nobody knows anything
    about the context these changes come from. -->
    <!--- Even if we are all from our internal team, we may not be on the
    same page. -->
    <!--- Write this PR as you were contributing to a public OSS project,
    where nobody knows you and you have to earn their trust. -->
    <!--- This will improve our projects in the long run! Thanks. -->
    
    #### List of Changes
    <!--- Describe your changes in detail -->
    Fix typo and update `package.json`.
    
    #### Motivation and Context
    <!--- Why is this change required? What problem does it solve? -->
    
    #### How Has This Been Tested?
    <!--- Please describe in detail how you tested your changes. -->
    <!--- Include details of your testing environment, tests ran to see how
    -->
    <!--- your change affects other areas of the code, etc. -->
    Unit tests.
    
    #### Screenshots (if appropriate):
    
    #### Types of changes
    <!--- What types of changes does your code introduce? Put an `x` in all
    the boxes that apply: -->
    - [x] Chore (nothing changes by a user perspective)
    - [x] Bug fix (non-breaking change which fixes an issue)
    - [ ] New feature (non-breaking change which adds functionality)
    - [ ] Breaking change (fix or feature that would cause existing
    functionality to not work as expected)
    
    #### Checklist:
    <!--- Go over all the following points, and put an `x` in all the boxes
    that apply. -->
    <!--- If you're unsure about any of these, don't hesitate to ask. We're
    here to help! -->
    - [x] My change requires a changeset, which I've added.
    - [ ] My change requires a change to the documentation.
    - [ ] I have updated the documentation accordingly.

---
+++ .changeset/eleven-hotels-drum.md
@@ -0,0 +1,5 @@
+---
+"@pagopa/io-auth-n-identity-commons": patch
+---
+
+`package.json` `require` section update and `EventTypeEnum` typo fix
--- packages/io-auth-n-identity-commons/src/types/auth-session-event.ts
+++ packages/io-auth-n-identity-commons/src/types/auth-session-event.ts
@@ -3,25 +3,25 @@ import { FiscalCode } from "@pagopa/ts-commons/lib/strings";
 import { enumType } from "@pagopa/ts-commons/lib/types";
 import * as t from "io-ts";
 
-export enum EventTypeEunum {
+export enum EventTypeEnum {
   "LOGIN" = "login",
   "LOGOUT" = "logout",
 }
 
 export type EventType = t.TypeOf<typeof EventType>;
-export const EventType = enumType<EventTypeEunum>(EventTypeEunum, "EventType");
+export const EventType = enumType<EventTypeEnum>(EventTypeEnum, "EventType");
 
 const BaseAuthSessionEvent = t.type({
   fiscalCode: FiscalCode,
 });
 
-//TODO: LoginEvent and LogoutEvent will contain other data,
+// TODO: LoginEvent and LogoutEvent will contain other data,
 // currently only the fields needed for user engagement are included,
 // under definition what to include in this event for other purposes
 
 export const LoginEvent = t.intersection([
   t.type({
-    eventType: t.literal(EventTypeEunum.LOGIN),
+    eventType: t.literal(EventTypeEnum.LOGIN),
     expiredAt: DateFromTimestamp,
   }),
   BaseAuthSessionEvent,
@@ -29,7 +29,7 @@ export const LoginEvent = t.intersection([
 export type LoginEvent = t.TypeOf<typeof LoginEvent>;
 
 export const LogoutEvent = t.intersection([
-  t.type({ eventType: t.literal(EventTypeEunum.LOGOUT) }),
+  t.type({ eventType: t.literal(EventTypeEnum.LOGOUT) }),
   BaseAuthSessionEvent,
 ]);
 export type LogoutEvent = t.TypeOf<typeof LogoutEvent>;
