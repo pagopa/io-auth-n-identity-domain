fbdfda1afe5a9dbe6c6ff6a05f1e18996b48b0f6
Gianmaria Scorza <gianmaria@scorza.dev>
2025-06-11 13:34:00 +0200

    add SessionNotificationEventsProcessorConfig

--- apps/io-profile-async/src/config.ts
+++ apps/io-profile-async/src/config.ts
@@ -47,6 +47,17 @@ export type ExpiredSessionDiscovererConfig = t.TypeOf<
   typeof ExpiredSessionDiscovererConfig
 >;
 
+export const SessionNotificationEventsProcessorConfig = t.type({
+  SESSION_NOTIFICATION_EVENTS_PROCESSOR_TTL_OFFSET: withFallback(
+    NumberFromString,
+    432000 // 5 days in seconds
+  )
+});
+
+export type SessionNotificationEventsProcessorConfig = t.TypeOf<
+  typeof SessionNotificationEventsProcessorConfig
+>;
+
 // global app configuration
 export type IConfig = t.TypeOf<typeof IConfig>;
 export const IConfig = t.intersection([
@@ -87,7 +98,8 @@ export const IConfig = t.intersection([
     MailerConfig,
     DryRunFeatureFlag,
     ExpiredSessionDiscovererConfig
-  ])
+  ]),
+  SessionNotificationEventsProcessorConfig
 ]);
 
 // No need to re-evaluate this object for each call
