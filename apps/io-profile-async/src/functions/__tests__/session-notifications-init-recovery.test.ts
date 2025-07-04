8dec30c25c00496bf063c7766c0782b639c642f5
Gianmaria Scorza <gianmaria@scorza.dev>
2025-07-04 14:26:26 +0200

    [#IOPID-2917] Adding SessionNotificationsInitRecovery QueueTrigger (#367)
    <!--- Please always add a PR description as if nobody knows anything
    about the context these changes come from. -->
    <!--- Even if we are all from our internal team, we may not be on the
    same page. -->
    <!--- Write this PR as you were contributing to a public OSS project,
    where nobody knows you and you have to earn their trust. -->
    <!--- This will improve our projects in the long run! Thanks. -->
    Adding `SessionNotificationsInitRecovery`, a QueueTrigger needed in
    order to recover sessions created previously the Auth ServiceBus events
    are activated.
    
    depends on https://github.com/pagopa/io-infra/pull/1574
    #### List of Changes
    <!--- Describe your changes in detail -->
    
    #### Motivation and Context
    <!--- Why is this change required? What problem does it solve? -->
    
    #### How Has This Been Tested?
    <!--- Please describe in detail how you tested your changes. -->
    <!--- Include details of your testing environment, tests ran to see how
    -->
    <!--- your change affects other areas of the code, etc. -->
    
    #### Screenshots (if appropriate):
    
    #### Types of changes
    <!--- What types of changes does your code introduce? Put an `x` in all
    the boxes that apply: -->
    - [ ] Chore (nothing changes by a user perspective)
    - [ ] Bug fix (non-breaking change which fixes an issue)
    - [ ] New feature (non-breaking change which adds functionality)
    - [ ] Breaking change (fix or feature that would cause existing
    functionality to not work as expected)
    
    #### Checklist:
    <!--- Go over all the following points, and put an `x` in all the boxes
    that apply. -->
    <!--- If you're unsure about any of these, don't hesitate to ask. We're
    here to help! -->
    - [ ] My change requires a changeset, which I've added.
    - [ ] My change requires a change to the documentation.
    - [ ] I have updated the documentation accordingly.

---
+++ apps/io-profile-async/src/functions/__tests__/session-notifications-init-recovery.test.ts
@@ -0,0 +1,371 @@
+/* eslint-disable max-lines-per-function */
+import { ValidationError } from "@pagopa/handler-kit";
+import { FiscalCode } from "@pagopa/ts-commons/lib/strings";
+import * as E from "fp-ts/lib/Either";
+import * as RTE from "fp-ts/lib/ReaderTaskEither";
+import { afterEach, describe, expect, it, vi } from "vitest";
+import { CosmosErrors } from "@pagopa/io-functions-commons/dist/src/utils/cosmosdb_model";
+import { SessionNotificationsRepositoryConfig } from "../../config";
+import { Client as SessionManagerInternalClient } from "../../generated/definitions/sm-internal/client";
+import { UserSessionInfo } from "../../generated/definitions/sm-internal/UserSessionInfo";
+import { SessionNotificationsModel } from "../../models/session-notifications";
+import { SessionNotificationsRepository } from "../../repositories/session-notifications";
+import { ExpiredSessionAdvisorQueueMessage } from "../../types/expired-session-advisor-queue-message";
+import * as appinsights from "../../utils/appinsights";
+import { mockQueueHandlerInputMocks } from "../__mocks__/handler.mock";
+import {
+  anEmptyAsyncIterable,
+  mockSessionNotificationsRepository
+} from "../__mocks__/session-notifications-repository.mock";
+import { SessionNotificationsInitRecoveryHandler } from "../session-notifications-init-recovery";
+import { PermanentError, TransientError } from "../../utils/errors";
+
+const aFiscalCode = "BBBBBB00B00B000B" as FiscalCode;
+const anExpiredAtDate = new Date();
+const aValidQueueMessage = {
+  fiscalCode: aFiscalCode,
+  expiredAt: anExpiredAtDate.getTime()
+};
+
+// Response Mocks
+const aValidGetSessionResponse: UserSessionInfo = {
+  active: true
+};
+
+// API Client Methods Mocks
+const getSessionMock = vi.fn(async () =>
+  E.of({
+    status: 200,
+    value: aValidGetSessionResponse
+  })
+);
+
+// Dependencies Mocks
+const mockSessionManagerInternalClient = ({
+  getSession: getSessionMock
+} as unknown) as SessionManagerInternalClient;
+
+const sessionNotificationsRepositoryConfigMock = {
+  SESSION_NOTIFICATION_EVENTS_TTL_OFFSET: 432000, // 5 days in seconds
+  SESSION_NOTIFICATION_EVENTS_FETCH_CHUNK_SIZE: 100
+} as SessionNotificationsRepositoryConfig;
+
+const mockSessionsNotificationModel = ({
+  buildAsyncIterable: vi.fn(),
+  patch: vi.fn(),
+  create: vi.fn(),
+  delete: vi.fn()
+} as unknown) as SessionNotificationsModel;
+
+const SessionNotificationsRepo = mockSessionNotificationsRepository;
+
+const trackEventMock = vi.spyOn(appinsights, "trackEvent");
+
+const makeHandlerInputs = (input: unknown) => ({
+  ...mockQueueHandlerInputMocks(
+    ExpiredSessionAdvisorQueueMessage,
+    input as ExpiredSessionAdvisorQueueMessage
+  ),
+  sessionManagerInternalClient: mockSessionManagerInternalClient,
+  sessionNotificationsRepositoryConfig: sessionNotificationsRepositoryConfigMock,
+  SessionNotificationsRepo: (SessionNotificationsRepo as unknown) as SessionNotificationsRepository,
+  sessionNotificationsModel: mockSessionsNotificationModel
+});
+
+describe("SessionNotificationsInitRecovery handler", () => {
+  afterEach(() => {
+    vi.clearAllMocks();
+  });
+
+  it("should succeed on user with an active session having no record on cosmosDB container", async () => {
+    SessionNotificationsRepo.findByFiscalCodeAsyncIterable.mockReturnValueOnce(
+      anEmptyAsyncIterable
+    );
+
+    const response = await SessionNotificationsInitRecoveryHandler({
+      ...makeHandlerInputs(aValidQueueMessage)
+    })();
+
+    expect(E.isRight(response)).toBe(true);
+    expect(getSessionMock).toHaveBeenCalledOnce();
+    expect(getSessionMock).toHaveBeenCalledWith({
+      fiscalCode: aValidQueueMessage.fiscalCode
+    });
+
+    expect(
+      SessionNotificationsRepo.findByFiscalCodeAsyncIterable
+    ).toHaveBeenCalledOnce();
+    expect(
+      SessionNotificationsRepo.findByFiscalCodeAsyncIterable
+    ).toHaveBeenCalledWith(aValidQueueMessage.fiscalCode);
+
+    expect(SessionNotificationsRepo.createRecord).toHaveBeenCalledOnce();
+    expect(SessionNotificationsRepo.createRecord).toHaveBeenCalledWith(
+      aValidQueueMessage.fiscalCode,
+      aValidQueueMessage.expiredAt
+    );
+  });
+
+  describe("Input Validation Failures", () => {
+    it("should fail when a bad Message is received", async () => {
+      const response = await SessionNotificationsInitRecoveryHandler({
+        ...makeHandlerInputs({ badProp: "bad" })
+      })();
+
+      expect(response).toStrictEqual(E.left(new ValidationError([])));
+      expect(getSessionMock).not.toHaveBeenCalled();
+      expect(
+        SessionNotificationsRepo.findByFiscalCodeAsyncIterable
+      ).not.toHaveBeenCalled();
+      expect(SessionNotificationsRepo.createRecord).not.toHaveBeenCalled();
+    });
+
+    it("should fail when a bad FiscalCode is received", async () => {
+      const response = await SessionNotificationsInitRecoveryHandler({
+        ...makeHandlerInputs({
+          ...aValidQueueMessage,
+          fiscalCode: "bad"
+        })
+      })();
+
+      expect(response).toStrictEqual(E.left(new ValidationError([])));
+      expect(getSessionMock).not.toHaveBeenCalled();
+      expect(
+        SessionNotificationsRepo.findByFiscalCodeAsyncIterable
+      ).not.toHaveBeenCalled();
+      expect(SessionNotificationsRepo.createRecord).not.toHaveBeenCalled();
+    });
+
+    it("should fail when a bad expiredAt is received", async () => {
+      const response = await SessionNotificationsInitRecoveryHandler({
+        ...makeHandlerInputs({
+          ...aValidQueueMessage,
+          expiredAt: "bad"
+        })
+      })();
+
+      expect(response).toStrictEqual(E.left(new ValidationError([])));
+      expect(getSessionMock).not.toHaveBeenCalled();
+      expect(
+        SessionNotificationsRepo.findByFiscalCodeAsyncIterable
+      ).not.toHaveBeenCalled();
+      expect(SessionNotificationsRepo.createRecord).not.toHaveBeenCalled();
+    });
+  });
+
+  // This test is to check QueuePermanentError handling
+  // on QueuePermanentError the response should be a right in order to not Retry the message
+  describe("PermanentError", () => {
+    const baseCustomEvent = {
+      name:
+        "io.citizen-auth.prof-async.session-notification-init-recovery.permanent",
+      properties: {},
+      tagOverrides: {
+        samplingEnabled: "false"
+      }
+    };
+    it("should fail silently on user without an active session with no error forwarded", async () => {
+      getSessionMock.mockImplementationOnce(async () =>
+        E.of({
+          status: 200,
+          value: { active: false }
+        })
+      );
+      const response = await SessionNotificationsInitRecoveryHandler({
+        ...makeHandlerInputs(aValidQueueMessage)
+      })();
+
+      expect(E.isRight(response)).toBe(true);
+      expect(getSessionMock).toHaveBeenCalledOnce();
+      expect(getSessionMock).toHaveBeenCalledWith({ fiscalCode: aFiscalCode });
+      expect(
+        SessionNotificationsRepo.findByFiscalCodeAsyncIterable
+      ).not.toHaveBeenCalled();
+      expect(SessionNotificationsRepo.createRecord).not.toHaveBeenCalled();
+      expect(trackEventMock).toHaveBeenCalledOnce();
+      expect(trackEventMock).toHaveBeenCalledWith({
+        ...baseCustomEvent,
+        properties: {
+          message: "User has no active session"
+        }
+      });
+    });
+
+    it("should fail silently on user which has already a record on CosmosDB Container", async () => {
+      const response = await SessionNotificationsInitRecoveryHandler({
+        ...makeHandlerInputs(aValidQueueMessage)
+      })();
+
+      expect(E.isRight(response)).toBe(true);
+      expect(getSessionMock).toHaveBeenCalledOnce();
+      expect(getSessionMock).toHaveBeenCalledWith({ fiscalCode: aFiscalCode });
+
+      expect(
+        SessionNotificationsRepo.findByFiscalCodeAsyncIterable
+      ).toHaveBeenCalledOnce();
+      expect(
+        SessionNotificationsRepo.findByFiscalCodeAsyncIterable
+      ).toHaveBeenCalledWith(aValidQueueMessage.fiscalCode);
+
+      expect(SessionNotificationsRepo.createRecord).not.toHaveBeenCalled();
+
+      expect(trackEventMock).toHaveBeenCalledOnce();
+      expect(trackEventMock).toHaveBeenCalledWith({
+        ...baseCustomEvent,
+        properties: {
+          message: "User already has records in container"
+        }
+      });
+    });
+
+    it("should fail silently on PermanentError Creating new record", async () => {
+      SessionNotificationsRepo.findByFiscalCodeAsyncIterable.mockReturnValueOnce(
+        anEmptyAsyncIterable
+      );
+      const errorMessage =
+        "Unable to calculate New Record TTL, the reason was => value -1000 at root is not a valid [integer >= 0]";
+
+      SessionNotificationsRepo.createRecord.mockReturnValueOnce(
+        RTE.fromEither(E.left(new PermanentError(errorMessage)))
+      );
+
+      const response = await SessionNotificationsInitRecoveryHandler({
+        ...makeHandlerInputs(aValidQueueMessage)
+      })();
+
+      expect(E.isRight(response)).toBe(true);
+      expect(getSessionMock).toHaveBeenCalledOnce();
+      expect(getSessionMock).toHaveBeenCalledWith({ fiscalCode: aFiscalCode });
+
+      expect(
+        SessionNotificationsRepo.findByFiscalCodeAsyncIterable
+      ).toHaveBeenCalledOnce();
+      expect(
+        SessionNotificationsRepo.findByFiscalCodeAsyncIterable
+      ).toHaveBeenCalledWith(aValidQueueMessage.fiscalCode);
+
+      expect(SessionNotificationsRepo.createRecord).toHaveBeenCalledOnce();
+      expect(SessionNotificationsRepo.createRecord).toHaveBeenCalledWith(
+        aValidQueueMessage.fiscalCode,
+        aValidQueueMessage.expiredAt
+      );
+
+      expect(trackEventMock).toHaveBeenCalledOnce();
+      expect(trackEventMock).toHaveBeenCalledWith({
+        ...baseCustomEvent,
+        properties: {
+          message: errorMessage
+        }
+      });
+    });
+  });
+
+  describe("TransientError", () => {
+    it.each`
+      scenario                        | mockedImpl                                               | expectedError
+      ${"throw"}                      | ${new Error("Error")}                                    | ${"Error while calling the downstream component [retrieveSession]"}
+      ${"return a Left"}              | ${E.left(new ValidationError(["aaaa"]))}                 | ${"Unexpected response from backend internal [retrieveSession]"}
+      ${"return status code not 200"} | ${E.right({ ...aValidGetSessionResponse, status: 500 })} | ${"Error while retrieving user session: downstream component returned 500 [retrieveSession]"}
+    `(
+      "should return a TransientError when GetSession $scenario",
+      async ({ mockedImpl, expectedError }) => {
+        getSessionMock.mockImplementationOnce(async () => {
+          if (mockedImpl instanceof Error) throw mockedImpl;
+          return mockedImpl;
+        });
+        const response = await SessionNotificationsInitRecoveryHandler({
+          ...makeHandlerInputs(aValidQueueMessage)
+        })();
+
+        expect(response).toStrictEqual(
+          E.left(new TransientError(expectedError))
+        );
+        expect(getSessionMock).toHaveBeenCalledOnce();
+        expect(getSessionMock).toHaveBeenCalledWith({
+          fiscalCode: aValidQueueMessage.fiscalCode
+        });
+        expect(
+          SessionNotificationsRepo.findByFiscalCodeAsyncIterable
+        ).not.toHaveBeenCalled();
+        expect(SessionNotificationsRepo.createRecord).not.toHaveBeenCalled();
+      }
+    );
+
+    it("should return a TransientError when findByFiscalCodeAsyncIterable fails", async () => {
+      SessionNotificationsRepo.findByFiscalCodeAsyncIterable.mockReturnValueOnce(
+        (_deps: unknown) =>
+          (async function*() {
+            yield await Promise.reject(new Error("Simulated failure"));
+          })()
+      );
+      const response = await SessionNotificationsInitRecoveryHandler({
+        ...makeHandlerInputs(aValidQueueMessage)
+      })();
+
+      expect(
+        E.left(
+          new TransientError(
+            "Error retrieving session expirations, AsyncIterable fetch execution failure"
+          )
+        )
+      ).toStrictEqual(response);
+
+      expect(getSessionMock).toHaveBeenCalledOnce();
+      expect(getSessionMock).toHaveBeenCalledWith({ fiscalCode: aFiscalCode });
+
+      expect(
+        SessionNotificationsRepo.findByFiscalCodeAsyncIterable
+      ).toHaveBeenCalledOnce();
+      expect(
+        SessionNotificationsRepo.findByFiscalCodeAsyncIterable
+      ).toHaveBeenCalledWith(aValidQueueMessage.fiscalCode);
+
+      expect(SessionNotificationsRepo.createRecord).not.toHaveBeenCalled();
+      expect(trackEventMock).not.toHaveBeenCalled();
+    });
+
+    it("should return a TransientError when createRecord fails", async () => {
+      SessionNotificationsRepo.findByFiscalCodeAsyncIterable.mockReturnValueOnce(
+        anEmptyAsyncIterable
+      );
+
+      const error = ({
+        kind: "COSMOS_ERROR",
+        error: new Error("Simulated failure")
+      } as unknown) as CosmosErrors;
+      SessionNotificationsRepo.createRecord.mockReturnValueOnce(
+        RTE.fromEither(E.left(error))
+      );
+
+      const response = await SessionNotificationsInitRecoveryHandler({
+        ...makeHandlerInputs(aValidQueueMessage)
+      })();
+
+      expect(
+        E.left(
+          new TransientError(
+            "An Error occurred while creating a new session record"
+          )
+        )
+      ).toStrictEqual(response);
+
+      expect(getSessionMock).toHaveBeenCalledOnce();
+      expect(getSessionMock).toHaveBeenCalledWith({ fiscalCode: aFiscalCode });
+
+      expect(
+        SessionNotificationsRepo.findByFiscalCodeAsyncIterable
+      ).toHaveBeenCalledOnce();
+      expect(
+        SessionNotificationsRepo.findByFiscalCodeAsyncIterable
+      ).toHaveBeenCalledWith(aValidQueueMessage.fiscalCode);
+
+      expect(SessionNotificationsRepo.createRecord).toHaveBeenCalledOnce();
+      expect(SessionNotificationsRepo.createRecord).toHaveBeenCalledWith(
+        aValidQueueMessage.fiscalCode,
+        aValidQueueMessage.expiredAt
+      );
+
+      expect(trackEventMock).not.toHaveBeenCalled();
+    });
+  });
+});
