5e829d23a7e0bd994c7a8fe259cfa76ee6854a57
Salvatore Laiso <32564922+salvatorelaiso@users.noreply.github.com>
2025-08-05 09:16:24 +0200

    [#IOPID-3168] Store maintenance connection string in KeyVault (#457)
    <!--- Please always add a PR description as if nobody knows anything
    about the context these changes come from. -->
    <!--- Even if we are all from our internal team, we may not be on the
    same page. -->
    <!--- Write this PR as you were contributing to a public OSS project,
    where nobody knows you and you have to earn their trust. -->
    <!--- This will improve our projects in the long run! Thanks. -->
    
    #### List of Changes
    <!--- Describe your changes in detail -->
    - Export maintenance storage primary connection string
    - Store the maintenance storage primary connection string inside the key
    vault
    
    #### Motivation and Context
    <!--- Why is this change required? What problem does it solve? -->
    
    #### How Has This Been Tested?
    <!--- Please describe in detail how you tested your changes. -->
    <!--- Include details of your testing environment, tests ran to see how
    -->
    <!--- your change affects other areas of the code, etc. -->
    
    #### Screenshots (if appropriate):
    
    #### Types of changes
    <!--- What types of changes does your code introduce? Put an `x` in all
    the boxes that apply: -->
    - [x] Chore (nothing changes by a user perspective)
    - [ ] Bug fix (non-breaking change which fixes an issue)
    - [ ] New feature (non-breaking change which adds functionality)
    - [ ] Breaking change (fix or feature that would cause existing
    functionality to not work as expected)
    
    #### Checklist:
    <!--- Go over all the following points, and put an `x` in all the boxes
    that apply. -->
    <!--- If you're unsure about any of these, don't hesitate to ask. We're
    here to help! -->
    - [ ] My change requires a changeset, which I've added.
    - [ ] My change requires a change to the documentation.
    - [ ] I have updated the documentation accordingly.

--- infra/resources/modules/storage_accounts/outputs.tf
+++ infra/resources/modules/storage_accounts/outputs.tf
@@ -19,9 +19,10 @@ output "audit" {
 
 output "maintenance" {
   value = {
-    id                  = module.st_maintenance_01.id
-    name                = module.st_maintenance_01.name
-    resource_group_name = module.st_maintenance_01.resource_group_name
-    principal_id        = module.st_maintenance_01.principal_id
+    id                        = module.st_maintenance_01.id
+    name                      = module.st_maintenance_01.name
+    resource_group_name       = module.st_maintenance_01.resource_group_name
+    principal_id              = module.st_maintenance_01.principal_id
+    primary_connection_string = module.st_session_01.primary_connection_string
   }
 }
