4132c44056679669a682f64c7784aac912acb12f
salvatorelaiso <salvatorelaiso@gmail.com>
2025-09-15 15:12:03 +0200

    Add primary connection string output for audit and new secret for audit connection string

--- infra/resources/modules/storage_accounts/outputs.tf
+++ infra/resources/modules/storage_accounts/outputs.tf
@@ -10,10 +10,11 @@ output "session" {
 
 output "audit" {
   value = {
-    id                  = module.st_audit_01.id
-    name                = module.st_audit_01.name
-    resource_group_name = module.st_audit_01.resource_group_name
-    principal_id        = module.st_audit_01.principal_id
+    id                        = module.st_audit_01.id
+    name                      = module.st_audit_01.name
+    resource_group_name       = module.st_audit_01.resource_group_name
+    principal_id              = module.st_audit_01.principal_id
+    primary_connection_string = module.st_audit_01.primary_connection_string
   }
 }
 
