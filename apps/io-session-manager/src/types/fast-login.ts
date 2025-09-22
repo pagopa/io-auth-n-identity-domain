f6036c9e8b4712759d47e6bb5e4803c9c574273a
Gianmaria Scorza <gianmaria@scorza.dev>
2025-09-22 03:41:13 +0200

    add comment explaining ho spid-common handle AdditionalLoginProp

--- apps/io-session-manager/src/types/fast-login.ts
+++ apps/io-session-manager/src/types/fast-login.ts
@@ -9,6 +9,9 @@ export enum LoginTypeEnum {
 export type LoginTypeT = t.TypeOf<typeof LoginType>;
 export const LoginType = enumType<LoginTypeEnum>(LoginTypeEnum, "LoginType");
 
+// Additional properties that can be passed to the login endpoint and propagated to the assertionConsumerService
+// Pay attention: on decode failure the error will be ignored, nothing will be propagated to the assertionConsumerService,
+// but the login still succeeds, https://github.com/pagopa/io-spid-commons/blob/0e80cb12ab8140cc5160be7e09aa419e60c7e012/src/strategy/saml_client.ts#L143
 export type AdditionalLoginPropsT = t.TypeOf<typeof AdditionalLoginProps>;
 export const AdditionalLoginProps = t.partial({
   loginType: LoginType,
