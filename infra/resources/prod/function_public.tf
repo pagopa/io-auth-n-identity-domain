c00d2deb85116d98314ae7deffc913cd31e94c3e
salvatorelaiso <salvatorelaiso@gmail.com>
2025-10-15 15:54:37 +0200

    feat: add maintenance storage account connection string and validation tokens table name to app settings

--- infra/resources/prod/function_public.tf
+++ infra/resources/prod/function_public.tf
@@ -26,6 +26,9 @@ locals {
 
       VALIDATION_CALLBACK_URL = "https://api-app.io.pagopa.it/email_verification.html"
       CONFIRM_CHOICE_PAGE_URL = "https://api-app.io.pagopa.it/email_confirm.html"
+
+      MAINTENANCE_STORAGE_ACCOUNT_CONNECTION_STRING = "@Microsoft.KeyVault(SecretUri=${azurerm_key_vault_secret.maintenance_st_connection_string.versionless_id})"
+      VALIDATION_TOKENS_TABLE_NAME                  = local.validation_tokens_table_name
     }
 
     # Using 100% sampling for production slot since the amount of traffic is
