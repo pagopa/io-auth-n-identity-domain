838ed45640f183afa54cd695d987d9a1a17b8c47
salvatorelaiso <salvatorelaiso@gmail.com>
2025-10-24 15:37:48 +0200

    fix: revert activation

--- infra/resources/prod/function_profile_async.tf
+++ infra/resources/prod/function_profile_async.tf
@@ -159,7 +159,7 @@ module "function_profile_async" {
       "AzureWebJobs.ExpiredSessionAdvisor.Disabled"              = "0",
       "AzureWebJobs.MigrateServicePreferenceFromLegacy.Disabled" = "0",
       "AzureWebJobs.OnProfileUpdate.Disabled"                    = "0", // TODO: remove after ITN migration
-      "AzureWebJobs.OnProfileUpdateItn.Disabled"                 = "0",
+      "AzureWebJobs.OnProfileUpdateItn.Disabled"                 = "1", // TODO: set to 0 to enable ITN migration
       "AzureWebJobs.StoreSpidLogs.Disabled"                      = "0",
       "AzureWebJobs.SessionNotificationEventsProcessor.Disabled" = "0"
     }
