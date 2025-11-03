0c3e8ad952de129519e5da6fbfe8c58a7602d243
salvatorelaiso <salvatorelaiso@gmail.com>
2025-11-03 15:11:04 +0100

    refactor: remove duplicated default values from config fields

--- packages/utilities/user-data-processing-invoker/config.ts
+++ packages/utilities/user-data-processing-invoker/config.ts
@@ -1,19 +1,12 @@
 import { NonEmptyString } from "@pagopa/ts-commons/lib/strings";
-import { withDefault } from "@pagopa/ts-commons/lib/types";
 import * as t from "io-ts";
 
 export const Config = t.type({
   apiUrl: NonEmptyString,
   apiKey: NonEmptyString,
-  inputFilePath: withDefault(NonEmptyString, "users.txt" as NonEmptyString),
-  invalidFiscalCodesFilePath: withDefault(
-    NonEmptyString,
-    "invalid_fiscal_codes.txt" as NonEmptyString,
-  ),
-  errorsFiscalCodesFilePath: withDefault(
-    NonEmptyString,
-    "errors_fiscal_codes.txt" as NonEmptyString,
-  ),
+  inputFilePath: NonEmptyString,
+  invalidFiscalCodesFilePath: NonEmptyString,
+  errorsFiscalCodesFilePath: NonEmptyString,
 });
 
 export type Config = t.TypeOf<typeof Config>;
