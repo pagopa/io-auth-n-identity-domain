77f6f43094c95d3bcfb571b34ddc4a2a514f3991
arcogabbo <arcogabbo@gmail.com>
2025-12-02 10:53:29 +0100

    added new package

---
+++ packages/io-auth-n-identity-db-models/.eslintrc.js
@@ -0,0 +1,13 @@
+/** @type {import("eslint").Linter.Config} */
+module.exports = {
+  env: {
+    es2021: true,
+    node: true,
+  },
+  extends: ["eslint-config-monorepo/index.js"],
+  parserOptions: {
+    project: "./tsconfig.eslint.json",
+    tsconfigRootDir: __dirname,
+  },
+  ignorePatterns: ["dist/**/*"],
+};
