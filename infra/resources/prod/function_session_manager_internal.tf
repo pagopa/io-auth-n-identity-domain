a62f569d3374c89f7ad44d81c6779eb1ad1f26b4
Mirco Civolani <mirco.civolani@outlook.it>
2025-12-22 16:53:18 +0100

    [#IOPID-3643] Scale-in fn sm int instances (#601)
    <!--- Please always add a PR description as if nobody knows anything
    about the context these changes come from. -->
    <!--- Even if we are all from our internal team, we may not be on the
    same page. -->
    <!--- Write this PR as you were contributing to a public OSS project,
    where nobody knows you and you have to earn their trust. -->
    <!--- This will improve our projects in the long run! Thanks. -->
    
    #### List of Changes
    <!--- Describe your changes in detail -->
    Scale-in fn-sm-internal instances from 15 to 10
    
    #### Motivation and Context
    <!--- Why is this change required? What problem does it solve? -->
    
    #### How Has This Been Tested?
    <!--- Please describe in detail how you tested your changes. -->
    <!--- Include details of your testing environment, tests ran to see how
    -->
    <!--- your change affects other areas of the code, etc. -->
    
    #### Screenshots (if appropriate):
    
    #### Types of changes
    <!--- What types of changes does your code introduce? Put an `x` in all
    the boxes that apply: -->
    - [ ] Chore (nothing changes by a user perspective)
    - [ ] Bug fix (non-breaking change which fixes an issue)
    - [ ] New feature (non-breaking change which adds functionality)
    - [ ] Breaking change (fix or feature that would cause existing
    functionality to not work as expected)
    
    #### Checklist:
    <!--- Go over all the following points, and put an `x` in all the boxes
    that apply. -->
    <!--- If you're unsure about any of these, don't hesitate to ask. We're
    here to help! -->
    - [ ] My change requires a changeset, which I've added.
    - [ ] My change requires a change to the documentation.
    - [ ] I have updated the documentation accordingly.

--- infra/resources/prod/function_session_manager_internal.tf
+++ infra/resources/prod/function_session_manager_internal.tf
@@ -122,8 +122,8 @@ module "function_session_manager_internal_autoscale" {
 
   scheduler = {
     normal_load = {
-      minimum = 15
-      default = 15
+      minimum = 10
+      default = 10
     },
     maximum = 30
   }
