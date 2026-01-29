bf6f07e52936e1151d322e9254ebd6394e9515d9
arcogabbo <arcogabbo@gmail.com>
2026-01-29 12:22:58 +0100

    edited permissions to include new foundation team

--- infra/repository/locals.tf
+++ infra/repository/locals.tf
@@ -4,7 +4,7 @@ locals {
     description              = "Auth&Identity Monorepo"
     topics                   = ["auth", "io"]
     jira_boards_ids          = ["CES", "IOPID"]
-    reviewers_teams          = ["io-auth-n-identity-backend", "engineering-team-devex"]
+    reviewers_teams          = ["io-auth-n-identity-backend", "io-foundation-identity-n-access-backend", "engineering-team-devex"]
     default_branch_name      = "main"
     infra_cd_policy_branches = ["main"]
     opex_cd_policy_branches  = ["main"]
