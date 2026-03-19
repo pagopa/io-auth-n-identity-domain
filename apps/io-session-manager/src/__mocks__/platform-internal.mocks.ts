db28dc982ff25fc7892ec98ed03caec901ea6bf5
salvatorelaiso <salvatorelaiso@gmail.com>
2026-03-19 16:20:51 +0100

    chore: remove comment

--- apps/io-session-manager/src/__mocks__/platform-internal.mocks.ts
+++ apps/io-session-manager/src/__mocks__/platform-internal.mocks.ts
@@ -3,8 +3,6 @@ import * as TE from "fp-ts/lib/TaskEither";
 import { PlatformInternalService } from "../services";
 
 export const mockCacheDelSessionToken = vi.fn().mockReturnValue(TE.right(true));
-// The following mocks a method with the signature: RTE.ReaderTaskEither<PlatformInternalClientDeps & AppInsightsDeps, Error, ReadonlyArray<true>>
-// It should return an array of true values, one for each session token to delete, based on the length of the input tokens array.
 export const mockCacheDelSessionTokens = vi.fn()
 .mockImplementation((tokens: ReadonlyArray<string>) => () => 
   TE.right(
