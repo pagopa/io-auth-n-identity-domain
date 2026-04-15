61e775bb95e7855d2486546083b369387216f0d4
arcogabbo <arcogabbo@gmail.com>
2026-04-15 11:58:10 +0200

    added unit tests

--- apps/io-profile/src/functions/__mocks__/durable-functions.ts
+++ apps/io-profile/src/functions/__mocks__/durable-functions.ts
@@ -1,23 +1,39 @@
 import { vi } from "vitest";
 import { InvocationContext } from "@azure/functions";
-import { OrchestrationRuntimeStatus } from "durable-functions";
+import {
+  DurableOrchestrationStatus,
+  OrchestrationRuntimeStatus,
+} from "durable-functions";
 
 export const mockStartNew = vi.fn(
   async (_name: string, _options?: { instanceId?: string; input?: unknown }) =>
     "00000000-0000-0000-0000-000000000000",
+);
+export const mockGetStatus = vi.fn(
+  async (id) =>
+    ({
+      name: "MOCKED",
+      instanceId: id,
+      createdTime: new Date(),
+      lastUpdatedTime: new Date(),
+      input: {},
+      output: {},
+      runtimeStatus: OrchestrationRuntimeStatus.Completed,
+    }) as DurableOrchestrationStatus,
 );
 
 export const mockGetClient = vi.fn(
   () =>
     ({
       startNew: mockStartNew,
+      getStatus: mockGetStatus,
     }) as any,
 );
 
 export const makeGetStatus404Error = (instanceId: string): Error =>
   new Error(
     `DurableClient error: Durable Functions extension replied with HTTP 404 response. ` +
-    `This usually means we could not find any data associated with the instanceId provided: ${instanceId}.`
+      `This usually means we could not find any data associated with the instanceId provided: ${instanceId}.`,
   );
 
 export const app = {
@@ -38,4 +54,3 @@ export const context = {
   warn: vi.fn().mockImplementation(console.warn),
   trace: vi.fn().mockImplementation(console.log),
 } as unknown as InvocationContext;
-
