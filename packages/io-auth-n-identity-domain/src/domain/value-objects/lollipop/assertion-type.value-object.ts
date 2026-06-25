17ea6cecd73e19d1c95910cd04a8fe046520acfd
salvatorelaiso <salvatorelaiso@gmail.com>
2026-06-25 10:55:28 +0200

    chore: remove lollipop value-objects (to be reintroduced)

--- packages/io-auth-n-identity-domain/src/domain/value-objects/lollipop/assertion-type.value-object.ts
+++
@@ -1,8 +0,0 @@
-import { z } from "zod";
-
-export const AssertionTypeSchema = z
-  .enum(["SAML", "OIDC"])
-  .describe("Assertion type, e.g. 'SAML' or 'OIDC'")
-  .brand<"AssertionType">();
-
-export type AssertionType = z.infer<typeof AssertionTypeSchema>;
