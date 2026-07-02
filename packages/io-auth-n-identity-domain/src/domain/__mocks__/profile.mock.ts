482cef61f7ae4705184aa00e5710ba7d52d4b3b4
arcogabbo <arcogabbo@gmail.com>
2026-07-02 10:25:33 +0200

    [#IOPID-4001] added unit tests

---
+++ packages/io-auth-n-identity-domain/src/domain/__mocks__/profile.mock.ts
@@ -0,0 +1,52 @@
+import {
+  EmailAddress,
+  FiscalCode,
+  NonEmptyString,
+} from "@pagopa/hexagonal-core";
+import {
+  ExtendedProfileSchema,
+  IsInboxEnabledSchema,
+  IsWebhookEnabledSchema,
+  PreferredLanguageSchema,
+  PreferredLanguagesEnum,
+  ServicePreferencesSettingsSchema,
+  ServicesPreferencesModeEnum,
+} from "../value-objects/profile/index.js";
+
+export const aCustomEmailAddress = "custom-email@example.com" as EmailAddress;
+
+export const anIsInboxEnabled = true as IsInboxEnabledSchema;
+export const anIsWebookEnabled = true as IsWebhookEnabledSchema;
+export const aPreferredLanguages: Array<PreferredLanguageSchema> = [
+  PreferredLanguagesEnum.it_IT,
+];
+export const aServicePreferencesSettings: ServicePreferencesSettingsSchema = {
+  mode: ServicesPreferencesModeEnum.AUTO,
+};
+
+export const aValidName = "Giuseppe Maria" as NonEmptyString;
+export const aValidFamilyname = "Garibaldi" as NonEmptyString;
+export const aFiscalCode = "ISPXNB32R82Y766D" as FiscalCode;
+
+export const mockedInitializedProfile = {
+  email: aCustomEmailAddress,
+  family_name: aValidFamilyname,
+  fiscal_code: aFiscalCode,
+  has_profile: true,
+  is_email_enabled: true,
+  is_email_validated: true,
+  is_email_already_taken: true,
+  is_inbox_enabled: anIsInboxEnabled,
+  is_webhook_enabled: anIsWebookEnabled,
+  name: aValidName,
+  preferred_languages: aPreferredLanguages,
+  service_preferences_settings: aServicePreferencesSettings,
+  version: 42,
+};
+
+export const mockedExtendedProfile: ExtendedProfileSchema = {
+  ...mockedInitializedProfile,
+  is_email_already_taken: false,
+  is_email_validated: true,
+  blocked_inbox_or_channels: {},
+};
