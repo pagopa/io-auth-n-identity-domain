d9279b6baa93c1d7b3206ee31604125cf5525cb7
salvatorelaiso <salvatorelaiso@gmail.com>
2026-07-06 09:59:26 +0200

    fix: update healthcheck return type to use GenericError

--- packages/io-auth-n-identity-domain/src/domain/ports/outbound/health-check.port.ts
+++ packages/io-auth-n-identity-domain/src/domain/ports/outbound/health-check.port.ts
@@ -1,5 +1,6 @@
+import { GenericError } from "@pagopa/hexagonal-core";
 import { Result } from "neverthrow";
 
 export interface HealthCheckOutboundPort {
-  healthcheck(): Promise<Result<void, Error>>;
+  healthcheck(): Promise<Result<void, GenericError>>;
 }
