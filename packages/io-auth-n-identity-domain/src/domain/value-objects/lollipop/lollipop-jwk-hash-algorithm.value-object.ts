29ddeba738b6b7adc84137588cf80e171ba12ce6
salvatorelaiso <salvatorelaiso@gmail.com>
2026-07-06 12:15:33 +0200

    refactor: update brand type usage in Lollipop assertion and hashing algorithm schemas

--- packages/io-auth-n-identity-domain/src/domain/value-objects/lollipop/lollipop-jwk-hash-algorithm.value-object.ts
+++ packages/io-auth-n-identity-domain/src/domain/value-objects/lollipop/lollipop-jwk-hash-algorithm.value-object.ts
@@ -1,12 +1,14 @@
 import { z } from "zod";
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+declare const BrandLollipopJwkHashAlgo: unique symbol;
 /**
  * Hashing algorithm used to compute the JWK thumbprint of the lollipop public key.
  */
 export const LollipopJwkHashingAlgorithmSchema = z
   .enum(["sha256", "sha384", "sha512"])
   .default("sha256")
-  .brand<"LollipopJwkHashAlgo">();
+  .brand<typeof BrandLollipopJwkHashAlgo>();
 
 export type LollipopJwkHashingAlgorithm = z.infer<
   typeof LollipopJwkHashingAlgorithmSchema
