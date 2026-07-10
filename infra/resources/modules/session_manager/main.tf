92cf6bd1f7b0ed8207ddddc758304da1218fca8e
Rodolfo Viti <rodolfo.viti@pagopa.it>
2026-07-10 08:48:31 +0000

    set required tf version to sm module

--- infra/resources/modules/session_manager/main.tf
+++ infra/resources/modules/session_manager/main.tf
@@ -1,16 +1,17 @@
 terraform {
+  required_version = ">= 1.14.0"
 
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~>4.7"
+      version = "~> 4.7"
     }
     dx = {
       source = "pagopa-dx/azure"
     }
     azapi = {
       source  = "azure/azapi"
-      version = "~>2.9"
+      version = "~> 2.9"
     }
   }
 }
