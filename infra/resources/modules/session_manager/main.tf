4be8dae86dbb48eb01b006a9677f02886b7b68c2
Rodolfo Viti <rodolfo.viti@pagopa.it>
2026-07-10 07:53:45 +0000

    update terraform

--- infra/resources/modules/session_manager/main.tf
+++ infra/resources/modules/session_manager/main.tf
@@ -2,7 +2,8 @@ terraform {
 
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
+      source  = "hashicorp/azurerm"
+      version = "~>4.7"
     }
     dx = {
       source = "pagopa-dx/azure"
