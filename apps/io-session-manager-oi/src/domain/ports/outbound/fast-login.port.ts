68cee9ed3c3c9c0f71c32ee0f46f2f4b245d4b9e
arcogabbo <arcogabbo@gmail.com>
2026-07-28 09:17:00 +0200

    fix: wrong import

--- apps/io-session-manager-oi/src/domain/ports/outbound/fast-login.port.ts
+++ apps/io-session-manager-oi/src/domain/ports/outbound/fast-login.port.ts
@@ -4,7 +4,7 @@ import {
   GenericError,
 } from "@pagopa/hexagonal-core";
 import { type Result } from "neverthrow";
-import { FastLoginParams } from "../../value-objects/fast-login.entity.js";
+import { FastLoginParams } from "../../value-objects/fast-login.vo.js";
 
 export interface FastLoginPort {
   fastLogin(
