df7719a4cd3bbc75b8ba3d2dbbda49009832a401
Rodolfo Viti <rodolfo.viti@pagopa.it>
2026-08-03 14:36:41 +0000

    restore add SM CA write permission to the new Azure Managd Redis

--- infra/resources/modules/session_manager/iam.tf
+++ infra/resources/modules/session_manager/iam.tf
@@ -39,11 +39,11 @@ module "ca_iam" {
     }
   ]
 
-  # managed_redis = [{
-  #   id          = module.managed_redis.id
-  #   role        = "writer"
-  #   description = "Allow Session Manager Container App to read/write to the Azure Managed Redis cache"
-  # }]
+  managed_redis = [{
+    id          = module.managed_redis.id
+    role        = "writer"
+    description = "Allow Session Manager Container App to read/write to the Azure Managed Redis cache"
+  }]
 
   # cosmos = [ # TODO: capire su quali DB necessita scrittura/lettura
   #   {
