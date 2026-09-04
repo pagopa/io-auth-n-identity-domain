a39e8d23b9779aa045f39e8429111802e4d28c42
Rodolfo Viti <rodolfo.viti@pagopa.it>
2026-09-04 15:46:07 +0000

    refactoring LollipopAssertionRefSchema

--- packages/io-auth-n-identity-domain/src/domain/value-objects/lollipop/lollipop-assertion-ref.vo.ts
+++ packages/io-auth-n-identity-domain/src/domain/value-objects/lollipop/lollipop-assertion-ref.vo.ts
@@ -2,15 +2,30 @@ import { z } from "zod";
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export declare const BrandLollipopAssertionRef: unique symbol;
+
+const assertionRefSchema = (algorithm: "sha256" | "sha384" | "sha512") =>
+  z.string().refine((value) => {
+    const prefix = `${algorithm}-`;
+    if (!value.startsWith(prefix)) {
+      return false;
+    }
+    return z
+      .hash(algorithm, { enc: "base64url" })
+      .safeParse(value.slice(prefix.length)).success;
+  }, `Invalid ${algorithm} assertion ref format`);
+
 /**
  * Lollipop assertion reference: a `{algo}-{base64url-thumbprint}` string
  * that uniquely identifies a reserved public key.
  */
 export const LollipopAssertionRefSchema = z
-  .string()
-  .regex(
-    /^(sha256-[A-Za-z0-9-_=]{1,44}|sha384-[A-Za-z0-9-_=]{1,66}|sha512-[A-Za-z0-9-_=]{1,88})$/,
-    { message: "Invalid assertion ref format" },
+  .union(
+    [
+      assertionRefSchema("sha256"),
+      assertionRefSchema("sha384"),
+      assertionRefSchema("sha512"),
+    ],
+    "Invalid assertion ref format",
   )
   .brand<typeof BrandLollipopAssertionRef>();
 
