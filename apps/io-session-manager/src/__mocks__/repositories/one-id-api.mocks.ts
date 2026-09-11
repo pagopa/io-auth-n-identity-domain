42e3710784e209e2d6ed0437e4ebd8d07911068b
arcogabbo <arcogabbo@gmail.com>
2026-09-11 17:52:49 +0200

    added service tests

--- apps/io-session-manager/src/__mocks__/repositories/one-id-api.mocks.ts
+++ apps/io-session-manager/src/__mocks__/repositories/one-id-api.mocks.ts
@@ -2,7 +2,9 @@ import { vi } from "vitest";
 import * as TE from "fp-ts/TaskEither";
 import { getOneIdAPIClient } from "../../repositories/one-id-api";
 
-export const mockGetSamlAssertion = vi.fn(() => TE.right("<xml/>"));
+export const mockGetSamlAssertion = vi.fn(() =>
+  TE.right<Error, string>("<xml/>"),
+);
 
 export const mockedOneIdAPIClient: ReturnType<typeof getOneIdAPIClient> = {
   getSamlAssertion: mockGetSamlAssertion,
