5d621370c7ae8b421b17fc1826bd9afa908819a5
Salvatore Laiso <32564922+salvatorelaiso@users.noreply.github.com>
2026-09-17 16:21:51 +0200

    BPD API proxy integration (#849)
    <!--- Please always add a PR description as if nobody knows anything
    about the context these changes come from. -->
    <!--- Even if we are all from our internal team, we may not be on the
    same page. -->
    <!--- Write this PR as you were contributing to a public OSS project,
    where nobody knows you and you have to earn their trust. -->
    <!--- This will improve our projects in the long run! Thanks. -->
    
    #### List of Changes
    <!--- Describe your changes in detail -->
    This pull request introduces the configuration for a new BPD (Bonus
    Pagamenti Digitali) API version 2 in the session manager module for API
    Management. The main changes involve defining resources for the new BPD
    API, associating it with the appropriate product, and tagging it for
    management and discoverability.
    
    **New BPD API configuration:**
    
    * Added a new `azurerm_api_management_api` resource named
    `bpd_api_session_manager_v2` to expose the BPD API (v2) via API
    Management, including its OpenAPI specification, description, and
    integration details.
    * Associated the new BPD API with the platform domain product using the
    `azurerm_api_management_product_api` resource, ensuring proper
    product-level access and management.
    * Tagged the new BPD API with the existing session manager tag using
    `azurerm_api_management_api_tag` for consistent resource classification
    and discoverability.
    
    **General structure:**
    
    * Added clear section headers (`EXTERNAL`, `BPD`) to organize the code
    and distinguish between different API configurations.
    
    #### Motivation and Context
    <!--- Why is this change required? What problem does it solve? -->
    Expose newly created BPD SSO API.
    
    
    #### How Has This Been Tested?
    <!--- Please describe in detail how you tested your changes. -->
    <!--- Include details of your testing environment, tests ran to see how
    -->
    <!--- your change affects other areas of the code, etc. -->
    
    #### Screenshots (if appropriate):
    
    #### Types of changes
    <!--- What types of changes does your code introduce? Put an `x` in all
    the boxes that apply: -->
    - [ ] Chore (nothing changes by a user perspective)
    - [ ] Bug fix (non-breaking change which fixes an issue)
    - [x] New feature (non-breaking change which adds functionality)
    - [ ] Breaking change (fix or feature that would cause existing
    functionality to not work as expected)
    
    #### Checklist:
    <!--- Go over all the following points, and put an `x` in all the boxes
    that apply. -->
    <!--- If you're unsure about any of these, don't hesitate to ask. We're
    here to help! -->
    - [x] My change requires a changeset, which I've added.
    - [ ] My change requires a change to the documentation.
    - [ ] I have updated the documentation accordingly.
    
    ---------
    
    Co-authored-by: Rodolfo Viti <62432865+rodoviti@users.noreply.github.com>

--- infra/resources/modules/platform_proxy_api/session_manager_v2.tf
+++ infra/resources/modules/platform_proxy_api/session_manager_v2.tf
@@ -1,3 +1,6 @@
+##################
+#    EXTERNAL    #
+##################
 resource "azurerm_api_management_api" "external_api_session_manager_v2" {
   name                  = "io-session-manager-external-api-v2"
   api_management_name   = var.platform_apim_name
@@ -33,3 +36,41 @@ resource "azurerm_api_management_api_tag" "external_api_session_manager_v2" {
   name   = azurerm_api_management_tag.session_manager_tag.name
 }
 
+
+##################
+#      BPD       #
+##################
+resource "azurerm_api_management_api" "bpd_api_session_manager_v2" {
+  name                  = "io-session-manager-bpd-api-v2"
+  api_management_name   = var.platform_apim_name
+  resource_group_name   = var.platform_apim_resource_group_name
+  subscription_required = false
+
+  version_set_id = azurerm_api_management_api_version_set.bpd_v1.id
+  version        = "v2"
+  revision       = 1
+
+  description  = "Auth & Identity Session Manager BPD API with OI integration"
+  display_name = "IO SESSION MANAGER OI BPD API"
+  path         = var.bpd_api_base_path
+  protocols    = ["https"]
+  service_url  = "${var.session_manager_oi_url}/${var.bpd_api_base_path}/v2"
+
+  import {
+    content_format = "openapi-link"
+    content_value  = "https://raw.githubusercontent.com/pagopa/io-auth-n-identity-domain/9b64e4db4341e8b0d2fd68a902917c12df5576d1/apps/io-session-manager-oi/api/sso/bpd.yaml"
+
+  }
+}
+
+resource "azurerm_api_management_product_api" "bpd_api_session_manager_v2" {
+  api_name            = azurerm_api_management_api.bpd_api_session_manager_v2.name
+  resource_group_name = var.platform_apim_resource_group_name
+  api_management_name = var.platform_apim_name
+  product_id          = data.azurerm_api_management_product.apim_platform_domain_product.product_id
+}
+
+resource "azurerm_api_management_api_tag" "bpd_api_session_manager_v2" {
+  api_id = azurerm_api_management_api.bpd_api_session_manager_v2.id
+  name   = azurerm_api_management_tag.session_manager_tag.name
+}
